f5c1d09c5c18f90c0f5629c27167f9fe48bd01da
Olivier Michel <webmaster@cyberbotics.com>
2019-09-05 21:10:06 +0200

    Fixed header

--- httpdocs/publish.php
+++ httpdocs/publish.php
@@ -2,7 +2,7 @@
 # Handling data in JSON format on the server-side using PHP
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Request-Headers: content-type");
+header("Access-Control-Allow-Headers: content-type");
 # build a PHP variable from JSON sent using POST method
 $v = json_decode(stripslashes(file_get_contents("php://input")));
 # encode the PHP variable to JSON and send it back on client-side
