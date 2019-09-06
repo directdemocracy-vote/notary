b61f389eef1da34185103bcd0ff9acaaa2242e1a
Galine <webmaster@cyberbotics.com>
2019-09-06 16:10:14 +0200

    Signature verifaction, JSON-schema validator

--- httpdocs/publish.php
+++ httpdocs/publish.php
@@ -1,12 +1,21 @@
 <?php
-# Handling data in JSON format on the server-side using PHP
+function error($message) {
+  die("{ \"error\": \"$message\" }");
+}
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
 # build a PHP variable from JSON sent using POST method
-$v = json_decode(stripslashes(file_get_contents("php://input")));
-# encode the PHP variable to JSON and send it back on client-side
-$signature = $v['signature'];
-echo $signature;
-#echo json_encode($v);
+$publication = json_decode(file_get_contents("php://input"));
+if (!$publication)
+  error("Unable to parse JSON post");
+if (!isset($publication->schema))
+  error("Unable to read schema field");
+$signature = base64_decode($publication->signature);
+$key = $publication->key;
+$publication->signature = '';
+$data = json_encode($publication, JSON_UNESCAPED_SLASHES);
+$verify = openssl_verify($data, $signature, $key, OPENSSL_ALGO_SHA256);
+if ($verify != 1)
+  error("Cannot verify signature ($verify)");
 ?>
