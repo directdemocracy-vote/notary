851d95ecb30b8bbc89e769cc1a3326b84a3ed76c
Galine <webmaster@cyberbotics.com>
2019-09-09 17:37:19 +0200

    Adjusted include path

--- httpdocs/publish.php
+++ httpdocs/publish.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 use Opis\JsonSchema\{
   IMediaType, MediaTypeContainer, Schema, Validator, ValidationResult, ValidationError
