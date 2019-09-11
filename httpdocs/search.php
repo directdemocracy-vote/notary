46df01ec710827dd26da4411ac33b3f8d9b236d5
Olivier Michel <license@cyberbotics.com>
2019-09-11 22:06:17 +0200

    Fixed

--- httpdocs/search.php
+++ httpdocs/search.php
@@ -8,20 +8,50 @@ function error($message) {
   die("{\"error\":$message}");
 }
 
+function get_float_parameter($name) {
+  if (isset($_GET[$name]))
+    return floatval($_GET[$name]);
+  if (isset($_POST[$name]))
+    return floatval($_POST[$name]);
+  return FALSE;
+}
+
+function get_string_parameter($name) {
+  if (isset($_GET[$name]))
+    return $_GET[$name];
+  if (isset($_POST[$name]))
+    return $_POST[$name];
+  return FALSE;
+}
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
-$latitude = floatval($_GET['latitude']) / 100000;
-$longitude = floatval($_GET['longitude']) / 100000;
-$range = floatval($_GET['range']) / 1000;
 $mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
 if ($mysqli->connect_errno)
   error("Failed to connect to MySQL database: $mysqli->connect_error ($mysqli->connect_errno)");
 $mysqli->set_charset('utf8mb4');
+
+$latitude = get_float_parameter('latitude') / 100000;
+$longitude = get_float_parameter('longitude') / 100000;
+$range = get_float_parameter('range') / 1000;
+$familyName = $mysqli->escape_string(get_string_parameter('familyName'));
+$givenNames = $mysqli->escape_string(get_string_parameter('givenNames'));
+
 $query = "SELECT givenNames, familyName, latitude, longitude, picture, "
         ."(6371 * acos(cos(radians(78.3232)) * cos(radians($latitude)) * cos(radians($longitude) - radians(65.3234)) "
-        ."+ sin(radians(78.3232)) * sin(radians($latitude)))) as distance FROM card HAVING distance < $range ORDER BY distance "
-        ."LIMIT 0, 20;";
+        ."+ sin(radians(78.3232)) * sin(radians($latitude)))) as distance FROM card HAVING distance < $range "
+if ($familyName || $givenNames) {
+  $query .= "WHERE ";
+  if ($familyName) {
+    $query .= "familyName LIKE '%$familyName%' ";
+    if ($givenNames)
+      $query .= "AND ";
+  }
+  if ($givenNames)
+    $query .= "givenNames LIKE '%$givenNames%' ";
+}
+$query .= "ORDER BY distance LIMIT 0, 20;";
 $result = $mysqli->query($query) or error($mysqli->error);
 $card = $result->fetch_assoc();
 $mysqli->close();
