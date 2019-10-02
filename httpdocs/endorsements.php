76ed4f0a1ff4d69f4f3e48ad0b19447e620e6d13
Olivier Michel <license@cyberbotics.com>
2019-10-02 22:19:13 +0200

    test

--- httpdocs/endorsements.php
+++ httpdocs/endorsements.php
@@ -10,10 +10,5 @@ if ($mysqli->connect_errno)
   die("{\"error\":\"Failed to connect to MySQL database: $mysqli->connect_error ($mysqli->connect_errno)\"}");
 $mysqli->set_charset('utf8mb4');
 $key = $mysqli->escape_string($_POST['key']);
-/*
-$query = "SELECT `key` FROM publication WHERE fingerprint='$fingerprint' AND `schema` LIKE '%/citizen.schema.json'";
-$result = $mysqli->query($query) or die("{\"error\":\"$mysqli->error\"}");
-$citizen = $result->fetch_assoc() or die("{\"error\":\"citizen not found: $query\"}");
-*/
 die(endorsements($mysqli, $key));
 ?>
