5fa4badf5473a053b987958773ee038e78248ef1
Olivier Michel <license@cyberbotics.com>
2019-10-02 22:50:20 +0200

    fix

--- httpdocs/citizen.php
+++ httpdocs/citizen.php
@@ -9,8 +9,9 @@ if ($mysqli->connect_errno)
   die("{\"error\":\"Failed to connect to MySQL database: $mysqli->connect_error ($mysqli->connect_errno)\"}");
 $mysqli->set_charset('utf8mb4');
 $key = $mysqli->escape_string($_POST['key']);
-$query = "SELECT publication.published, publication.expires, citizen.familyName, "
-        ."citizen.givenNames, citizen.picture, citizen.latitude, citizen.longitude "
+$query = "SELECT publication.published, publication.expires, publication.fingerprint, "
+        ."citizen.familyName, citizen.givenNames, citizen.picture, "
+        ."citizen.latitude, citizen.longitude "
         ."FROM publication INNER JOIN citizen ON publication.id = citizen.id "
         ."WHERE publication.`key` = '$key'";
 $result = $mysqli->query($query) or die("{\"error\":\"$mysqli->error\"}");
