09a4a53ceb40e579f16099a1a348cec940fb8e20
Olivier Michel <license@cyberbotics.com>
2019-10-03 16:10:40 +0200

    fixed

--- httpdocs/citizen.php
+++ httpdocs/citizen.php
@@ -23,8 +23,27 @@ settype($citizen['expires'], 'int');
 settype($citizen['latitude'], 'int');
 settype($citizen['longitude'], 'int');
 $endorsements = endorsements($mysqli, $key);
+$query = "SELECT pc.fingerprint, pe.published, e.revoke, "
+        ."c.familyName, c.givenNames, c.picture FROM "
+        ."publication pe INNER JOIN endorsement e ON pe.id = e.id, "
+        ."publication pc INNER JOIN citizen c ON pc.id = c.id "
+        ."WHERE e.publicationKey = '$key' AND pc.`key` = e.publicationKey "
+        ."AND pc.`signature` = e.publicationSignature "
+        ."ORDER BY e.revoke ASC, pe.published, c.familyName, c.givenNames";
+$result = $mysqli->query($query);
+if (!$result)
+  return "{\"error\":\"$mysqli->error\"}";
+$citizen_endorsements = array();
+while($e = $result->fetch_assoc()) {
+  settype($e['published'], 'int');
+  settype($e['revoke'], 'bool');
+  $citizen_endorsements[] = $e;
+}
+$result->free();
+$mysqli->close();
 $answer = array();
 $answer['citizen'] = $citizen;
 $answer['endorsements'] = $endorsements;
+$answer['citizen_endorsements'] = $citizen_endorsements;
 die(json_encode($answer, JSON_UNESCAPED_SLASHES));
 ?>
