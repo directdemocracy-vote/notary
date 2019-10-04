34040bf3cc70787d9e586bda459875c862c1673a
Olivier Michel <license@cyberbotics.com>
2019-10-04 22:42:57 +0200

    new

---
+++ httpdocs/coordinates.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../php/database.php';
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: content-type");
+$mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
+if ($mysqli->connect_errno)
+  die("{\"error\":\"Failed to connect to MySQL database: $mysqli->connect_error ($mysqli->connect_errno)\"}");
+$mysqli->set_charset('utf8mb4');
+$key = $mysqli->escape_string($_POST['key']);
+$query = "SELECT latitude, longitude FROM citizen INNER JOIN publication ON publication.id = citizen.id "
+        ."WHERE publication.`key` = '$key'";
+$result = $mysqli->query($query) or die("{\"error\":\"$mysqli->error\"}");
+$citizen = $result->fetch_assoc() or die("{\"error\":\"citizen not found: $key\"}");
+$result->free();
+settype($citizen['latitude'], 'int');
+settype($citizen['longitude'], 'int');
+$mysqli->close();
+die(json_encode($citizen, JSON_UNESCAPED_SLASHES));
+?>
