a10c71793383546d16afeac071cdd3aa4a8cd690
Olivier Michel <license@cyberbotics.com>
2019-10-23 22:12:36 +0200

    fix

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -192,10 +192,10 @@
                     a.forEach(function(c) {
                       const name = c.givenNames + ' ' + c.familyName;
                       console.log(name);
+                      const label = '<img src="' + c.picture + '" width=60 height=80><div>' + name + '</div>';
                       const lat = c.latitude / 1000000;
                       const lon = c.longitude / 1000000;
-                      var marker = L.marker([lat, lon]).addTo(map).bindPopup(name).openPopup();
-                      //marker.setPopupContent(name).openPopup();
+                      var marker = L.marker([lat, lon]).addTo(map).bindPopup(label).openPopup();
                     });
                   }
                 }
