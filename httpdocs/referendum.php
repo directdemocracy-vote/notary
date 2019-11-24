88fa134a78746ebe8b882775f35f57a91ff69e86
Olivier Michel <license@cyberbotics.com>
2019-11-24 18:15:02 +0100

    simplified

--- httpdocs/referendum.php
+++ httpdocs/referendum.php
@@ -14,27 +14,16 @@ $mysqli = new mysqli($database_host, $database_username, $database_password, $da
 if ($mysqli->connect_errno)
   die("{\"error\":\"Failed to connect to MySQL database: $mysqli->connect_error ($mysqli->connect_errno)\"}");
 $mysqli->set_charset('utf8mb4');
-$areas = json_decode(file_get_contents("php://input"));
-if (!$areas)
+$area = json_decode(file_get_contents("php://input"));
+if (!$area)
   error("Unable to parse JSON post");
-if (!$areas->reference)
-  error("Missing areas.reference field");
-$reference = $areas->reference;
 $query = "SELECT "
         ."publication.schema, publication.key, publication.signature, publication.published, publication.expires, "
-        ."referendum.trustee, referendum.id AS areas, referendum.title, referendum.description, "
+        ."referendum.trustee, referendum.area, referendum.title, referendum.description, "
         ."referendum.question, referendum.answers, referendum.deadline, referendum.website "
         ."FROM referendum "
         ."LEFT JOIN publication ON publication.id = referendum.id "
-        ."LEFT JOIN area ON area.parent = referendum.id "
-        ."WHERE area.reference=\"$reference\" AND (";
-foreach($areas->areas as $area) {
-  $type = $area->type;
-  $name = $area->name;
-  $query .= "(area.type=\"$type\" AND area.name=\"$name\") OR ";
-}
-$query = substr($query, 0, -4); // remove last " OR "
-$query .= ")";
+        ."WHERE \"$area\" LIKE CONCAT(publication.area, '%')";
 $result = $mysqli->query($query) or die("{\"error\":\"$mysqli->error\"}");
 $referendums = array();
 while ($referendum = $result->fetch_assoc()) {
@@ -42,12 +31,6 @@ while ($referendum = $result->fetch_assoc()) {
   settype($referendum['expires'], 'int');
   settype($referendum['areas'], 'int');
   settype($referendum['deadline'], 'int');
-  $q = "SELECT reference, type, name FROM area WHERE parent=$referendum[areas]";
-  $r = $mysqli->query($q) or die("{\"error\":\"$mysqli->error\"}");
-  $referendum['areas'] = array();
-  while ($area = $r->fetch_assoc())
-    $referendum['areas'][] = $area;
-  $r->free();
   $referendums[] = $referendum;
 }
 $result->free();
