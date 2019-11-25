106d36f0939da9da1cc3a1958abac82c883c7438
Olivier Michel <license@cyberbotics.com>
2019-11-25 22:10:17 +0100

    test

--- httpdocs/referendum.php
+++ httpdocs/referendum.php
@@ -23,8 +23,7 @@ $query = "SELECT "
         ."referendum.question, referendum.answers, referendum.deadline, referendum.website "
         ."FROM referendum "
         ."LEFT JOIN publication ON publication.id = referendum.id "
-        ."WHERE \"$area\" LIKE CONCAT(referendum.area, '%')";
-die($query);
+        ."WHERE SUBSTRING(\"$area\", 0, LENGTH(referendum.area) LIKE referendum.area";
 $result = $mysqli->query($query) or die("{\"error\":\"$mysqli->error\"}");
 $referendums = array();
 while ($referendum = $result->fetch_assoc()) {
