e62e3a6be7622ea69edf7751ec5d86f84c24e1cf
Galine <webmaster@cyberbotics.com>
2020-04-01 16:58:35 +0200

    test

--- httpdocs/trustee_url.php
+++ httpdocs/trustee_url.php
@@ -7,12 +7,12 @@ if ($mysqli->connect_errno)
 $mysqli->set_charset('utf8mb4');
 if (isset($_GET['referendum'])) {
   $referendum = $mysqli->escape_string($_GET['referendum']);
-  $referendum = replace(' ', '+', $referendum);
+  $referendum = str_replace(' ', '+', $referendum);
   $query = "SELECT trustee.url FROM trustee LEFT JOIN referendum ON referendum.trustee = trustee.`key` LEFT JOIN publication " .
            "ON publication.id = referendum.id WHERE publication.`key` = \"$referendum\"";
 } else if (isset($_GET['key'])) {
   $key = $mysqli->escape_string($_GET['key']);
-  $key = replace(' ', '+', $key);
+  $key = str_replace(' ', '+', $key);
   $query = "SELECT url FROM trustee WHERE `key` = \"$key\"";
 } else
   die("Missing key or referendum argument.");
