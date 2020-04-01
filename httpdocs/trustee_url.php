2c79abd3929109480ef438ca1f428f533310c292
Galine <webmaster@cyberbotics.com>
2020-04-01 16:25:22 +0200

    test

--- httpdocs/trustee_url.php
+++ httpdocs/trustee_url.php
@@ -5,11 +5,11 @@ $mysqli = new mysqli($database_host, $database_username, $database_password, $da
 if ($mysqli->connect_errno)
   die("Failed to connect to MySQL database: $mysqli->connect_error ($mysqli->connect_errno)");
 $mysqli->set_charset('utf8mb4');
-if (isset($_POST['referendum'])) {
-  $referendum = $mysqli->escape_string($_POST['referendum']);
+if (isset($_GET['referendum'])) {
+  $referendum = $mysqli->escape_string($_GET['referendum']);
   $query = "SELECT url FROM trustee WHERE `key` = (SELECT trustee FROM referendum WHERE `key` = $referendum)";
-} else if (isset($_POST['key'])) {
-  $key = $mysqli->escape_string($_POST['key']);
+} else if (isset($_GET['key'])) {
+  $key = $mysqli->escape_string($_GET['key']);
   $query = "SELECT url FROM trustee WHERE `key` = \"$key\"";
 } else
   die("Missing key or referendum argument.");
