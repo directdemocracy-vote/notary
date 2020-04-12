a3adbbb231c9c1d9ef3261079de9c13f958c296b
Galine <webmaster@cyberbotics.com>
2020-04-12 19:32:37 +0200

    fixed

--- httpdocs/registrations.php
+++ httpdocs/registrations.php
@@ -33,20 +33,40 @@ $query = "SELECT publication.`schema`, publication.`key`, publication.signature,
         ."FROM registration LEFT JOIN publication ON publication.id=registration.id "
         ."WHERE registration.referendum='$referendum' AND published <= $now AND expires >= $now";
 $result = $mysqli->query($query) or error($mysqli->error);
-if (!$result)
-  die('[]');
 $registrations = [];
-while ($registration = $result->fetch_assoc()) {
-  $registration['published'] = floatval($registration['published']);
-  $registration['expires'] = floatval($registration['expires']);
-  $station_key = $registration['stationKey'];
-  $station_signature = $registration['stationSignature'];
-  unset($registration['stationKey']);
-  unset($registration['stationSignature']);
-  $registration['station'] = array('key' => $station_key, 'signature' => $station_signature);
-  array_push($registrations, $registration);
+if ($results) {
+  while ($registration = $result->fetch_assoc()) {
+    $registration['published'] = floatval($registration['published']);
+    $registration['expires'] = floatval($registration['expires']);
+    $station_key = $registration['stationKey'];
+    $station_signature = $registration['stationSignature'];
+    unset($registration['stationKey']);
+    unset($registration['stationSignature']);
+    $registration['station'] = array('key' => $station_key, 'signature' => $station_signature);
+    array_push($registrations, $registration);
+  }
+  $result->free();
 }
-$result->free();
+$query = "SELECT publication.`schema`, publication.`key`, publication.signature, publication.published, publication.expires, "
+        ."ballot.referendum, ballot.stationKey, ballot.stationSignature "
+        ."FROM ballot LEFT JOIN publication ON publication.id=ballot.id "
+        ."WHERE ballot.referendum='$referendum' AND published <= $now AND expires >= $now";
+$result = $mysqli->query($query) or error($mysqli->error);
+$ballots = [];
+if ($results) {
+  while ($ballot = $result->fetch_assoc()) {
+    $ballot['published'] = floatval($ballot['published']);
+    $ballot['expires'] = floatval($ballot['expires']);
+    $station_key = $ballot['stationKey'];
+    $station_signature = $ballot['stationSignature'];
+    unset($ballot['stationKey']);
+    unset($ballot['stationSignature']);
+    $ballot['station'] = array('key' => $station_key, 'signature' => $station_signature);
+    array_push($ballots, $ballot);
+  }
+  $result->free();
+}
 $mysqli->close();
-echo json_encode($registrations, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+$response = array('registrations' => $registrations, 'ballots' => $ballots);
+echo json_encode($response, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 ?>
