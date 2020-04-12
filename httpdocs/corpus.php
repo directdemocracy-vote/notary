dd5221740648b8dd7f5714368a84d0e8b8cb4a47
Galine <webmaster@cyberbotics.com>
2020-04-12 22:22:42 +0200

    test

--- httpdocs/corpus.php
+++ httpdocs/corpus.php
@@ -39,9 +39,11 @@ $result->free();
 
 $areas = explode("\n", $referendum['area']);
 $area = '?';
-foreach($areas as $a)
-  $area .= urlencode($a) . '&';
-$url = "https://nominatim.openstreetmap.org/search?" . $area . "polygon_geojson=1&format=jsonv2";
+foreach($areas as $a) {
+  $n = explode("=", $a, 2);
+  $area .= $n[0] . '=' . urlencode($n[1]) . '&';
+}
+$url = "https://nominatim.openstreetmap.org/search" . $area . "polygon_geojson=1&format=jsonv2";
 die($url."<br>".urlencode($url));
 $area = file_get_contents($url);
 die($area);
