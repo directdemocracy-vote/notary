37544f2e0e3409824ea6bd01abbdde938fb0e9b2
Galine <webmaster@cyberbotics.com>
2020-04-13 21:23:38 +0200

    updated

--- httpdocs/coordinates.php
+++ httpdocs/coordinates.php
@@ -9,13 +9,13 @@ if ($mysqli->connect_errno)
   die("{\"error\":\"Failed to connect to MySQL database: $mysqli->connect_error ($mysqli->connect_errno)\"}");
 $mysqli->set_charset('utf8mb4');
 $key = $mysqli->escape_string($_POST['key']);
-$query = "SELECT latitude, longitude FROM citizen INNER JOIN publication ON publication.id = citizen.id "
+$query = "SELECT ST_Y(home) AS latitude, ST_X(home) AS longitude FROM citizen INNER JOIN publication ON publication.id = citizen.id "
         ."WHERE publication.`key` = '$key'";
 $result = $mysqli->query($query) or die("{\"error\":\"$mysqli->error\"}");
 $citizen = $result->fetch_assoc() or die("{\"error\":\"citizen not found: $key\"}");
 $result->free();
-settype($citizen['latitude'], 'int');
-settype($citizen['longitude'], 'int');
+settype($citizen['latitude'], 'float');
+settype($citizen['longitude'], 'float');
 $mysqli->close();
 die(json_encode($citizen, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
 ?>
