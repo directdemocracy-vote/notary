9b1b39d346ae42b4343c44a45c97d4103081a16b
Galine <webmaster@cyberbotics.com>
2020-04-17 12:36:01 +0200

    fix

--- httpdocs/publications.php
+++ httpdocs/publications.php
@@ -53,10 +53,7 @@ $condition .= "p.schema='https://directdemocracy.vote/json-schema/$version/$type
 
 $query = "SELECT p.`schema`, p.`key`, p.signature, p.published, p.expires, $fields FROM $type "
         ."LEFT JOIN publication AS p ON p.id=$type.id AND $condition WHERE p.id IS NOT NULL";
-/*
-$query = "SELECT p.`schema`, p.`key`, p.signature, p.published, p.expires, $fields FROM publication AS p "
-        ."LEFT JOIN $type ON $type.id=p.id AND $condition";
-*/
+
 $result = $mysqli->query($query) or error($mysqli->error);
 $publications = array();
 if ($result) {
