2bad2a6da3a4bebd4980c8be2a86e5f407718c20
Megatol <license@cyberbotics.com>
2020-12-02 22:41:09 +0100

    fix

--- httpdocs/participants.php
+++ httpdocs/participants.php
@@ -61,7 +61,8 @@ $query = "SELECT citizen.id, citizen.familyName, citizen.givenNames, citizen.pic
         ."citizen_publication.published, citizen_publication.expires "
         ."FROM citizen "
         ."LEFT JOIN publication AS citizen_publication ON citizen_publication.id=citizen.id "
-        ."LEFT JOIN registration ON registration.citizen=citizen.id AND registration.referendum=\"$referendum_key\" "
+        ."LEFT JOIN registration ON registration.referendum=\"$referendum_key\" "
+        ."LEFT JOIN publication AS registration_publication ON registration_publication.`key`=citizen_publication.`key` "
         ."WHERE CONTAINS($polygons, home) "
         ."ORDER BY familyName, givenNames";
 $result = $mysqli->query($query) or error($query . " - " . $mysqli->error);
