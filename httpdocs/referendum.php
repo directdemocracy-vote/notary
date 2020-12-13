6a04419670ececf1aa3fbfad62f09a25a253866b
Megatol <license@cyberbotics.com>
2020-12-13 17:02:13 +0100

    fix

--- httpdocs/referendum.php
+++ httpdocs/referendum.php
@@ -70,6 +70,7 @@ if (isset($fingerprint)) {
         $referendums[] = $referendum;
       }
       $result->free();
+      die($query);
     }
     $query = "$query_base WHERE referendum.area = \"$area_name\" AND referendum.deadline > (1000 * UNIX_TIMESTAMP()) ";
     if (isset($fingerprints))
