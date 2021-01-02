f2557e7484f4900ffa65ead00473718be5778336
Megatol <license@cyberbotics.com>
2021-01-02 18:30:52 +0100

    fix

--- httpdocs/js/citizen.js
+++ httpdocs/js/citizen.js
@@ -31,6 +31,7 @@ window.onload = function() {
       let col = document.createElement('div');
       row.appendChild(col);
       col.classList.add('col');
+      col.style.maxWidth = '150px';
       let img = document.createElement('img');
       col.appendChild(img);
       img.src = answer.citizen.picture;
