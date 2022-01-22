866a62e8fb9f99786b0dd3653ceb34fdf9909820
Megatol <license@cyberbotics.com>
2022-01-22 17:20:53 +0100

    fix

---
+++ httpdocs/js/trustee.js
@@ -0,0 +1,40 @@
+function findGetParameter(parameterName, result = null) {
+  location.search.substr(1).split("&").forEach(function(item) {
+    let tmp = item.split("=");
+    if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+  });
+  return result;
+}
+
+window.onload = function() {
+  let trustee = findGetParameter('trustee', 'https://trustee.directdemocracy.vote');
+  let content = document.getElementById('content');
+  let xhttp = new XMLHttpRequest();
+  xhttp.open('POST', '/trustee.php', true);
+  xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhttp.send('trustee=' + encodeURIComponent(trustee));
+  xhttp.onload = function() {
+    if (this.status == 200) {
+      let answer = JSON.parse(this.responseText);
+      if (answer.error) {
+        console.log(answer.error);
+        return;
+      }
+      for (i = 0; i < answer.endorsement.length; i++) {
+        let row = document.createElement('div');
+        content.appendChild(row);
+        row.classList.add('row');
+        let col = document.createElement('div');
+        row.appendChild(col);
+        col.classList.add('col');
+        col.innerHTML = new Date(answer.endorsement[i].published).toISOString.slice(0, 10);
+        col = document.createElement('div');
+        row.appendChild(col);
+        col.classList.add('col');
+        if (answer.endorsement[i].hasOwnProperty('revoke'))
+          col.style.textDecoration = 'line-through';
+        col.innerHTML = answer.endorsement[i].name;
+      }
+    }
+  };
+};
---
+++ httpdocs/trustee.php
@@ -0,0 +1,42 @@
+<?php
+require_once '../php/database.php';
+require_once '../php/endorsements.php';
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: content-type");
+$mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
+if ($mysqli->connect_errno)
+  die("{\"error\":\"Failed to connect to MySQL database: $mysqli->connect_error ($mysqli->connect_errno)\"}");
+$mysqli->set_charset('utf8mb4');
+if (isset($_POST['trustee']))
+  $trustee = $mysqli->escape_string($_POST['trustee']);
+else
+  $trustee = 'https://trustee.directdemocracy.vote';
+
+$query = "SELECT `key` FROM trustee WHERE url=\"$trustee\"";
+$result = $mysqli->query($query) or die("{\"error\":\"$mysqli->error\"}");
+$assoc = $result->fetch_assoc() or die("{\"error\":\"trustee not found: $trustee\"}");
+$result->free();
+$trustee_key = $assoc['key'];
+
+$query = "SELECT "
+        ."endorsement_p.published, endorsement_p.expires, endorsement.`revoke`, citizen.familyName, citizen.givenNames, "
+        ."FROM publication AS endorsement_p"
+        ."INNER JOIN endorsement ON endorsement.id = endorsement_p.id "
+        ."INNER JOIN publication AS citizen_p ON citizen_p.fingerprint = endorsement.publicationFingerprint "
+        ."INNER JOIN citizen ON citizen.id = citizen_p.id "
+        ."WHERE endorsement_p.`key`=\"$trustee_key\"";
+$result = $mysqli->query($query) or die("{\"error\":\"$mysqli->error\"}");
+$endorsements = array();
+while ($endorsement = $result->fetch_assoc()) {
+  settype($endorsement['published'], 'int');
+  settype($endorsement['expires'], 'int');
+  settype($endorsement['revoke'], 'bool');
+  $endorsements[] = $endorsement;
+}
+$result->free();
+$answer = array();
+$answer['endorsements'] = $endorsements;
+die(json_encode($answer, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+?>
