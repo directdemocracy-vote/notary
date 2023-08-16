dfe321946b40ff1fc7b3450e6b888697a0353bc6
megatol <74714212+megatol@users.noreply.github.com>
2023-08-16 18:17:01 +0200

    Update publish.php

--- httpdocs/publish.php
+++ httpdocs/publish.php
@@ -131,7 +131,7 @@ elseif ($type == 'endorsement') {
   $result->free();
   if ($endorsed && $endorsed['signature'] != $endorsement->endorsedSignature)
     error("endorsement signature mismatch");
-  $query = "INSERT INTO endorsement(id, endorsedFingerprint, revoke, message, comment, endorsedSignature) "
+  $query = "INSERT INTO endorsement(id, endorsedFingerprint, `revoke`, message, comment, endorsedSignature) "
           ."VALUES($id, SHA1(\"$endorsement->endorsedSignature\"), $endorsement->revoke, \"$endorsement->message\", \"$endorsement->comment\", \"$endorsement->endorsedSignature\")";
 } elseif ($type == 'proposal') {
   $proposal =&$publication;
