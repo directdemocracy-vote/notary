e03897af61116df9ecdeb1763f1781e5a3d794f8
megatol <74714212+megatol@users.noreply.github.com>
2023-08-18 10:22:28 +0200

    Update citizen.js

--- httpdocs/js/citizen.js
+++ httpdocs/js/citizen.js
@@ -7,7 +7,7 @@ function findGetParameter(parameterName, result = null) {
 }
 
 window.onload = function() {
-  let judge = findGetParameter('judge', 'https://trustee.directdemocracy.vote');
+  let judge = findGetParameter('judge', 'https://judge.directdemocracy.vote');
   const fingerprint = findGetParameter('fingerprint');
   if (!fingerprint) {
     console.log('Missing fingerprint GET argument.');
@@ -95,7 +95,7 @@ window.onload = function() {
         `<h4>Reputation: <span id="reputation">...</span></h4>
 <div class="input-group mb-3">
   <span class="input-group-text" id="protocol">https://</span>
-  <input id="judge" type="text" class="form-control" placeholder="https://trustee.directdemocracy.vote"
+  <input id="judge" type="text" class="form-control" placeholder="https://judge.directdemocracy.vote"
    aria-label="Judge URL" aria-describedby="protocol" value="${judge.substring(8)}">
    <button class="btn btn-outline-secondary" type="button" id="reload"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
   <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
