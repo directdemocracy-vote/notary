c500beab397c90ac74b8800f325f74f4426801bd
megatol <74714212+megatol@users.noreply.github.com>
2023-08-18 08:38:37 +0200

    Update and rename trustee_url.php to judge_url.php

--- httpdocs/judge_url.php
+++ httpdocs/judge_url.php
@@ -8,18 +8,18 @@ $mysqli->set_charset('utf8mb4');
 if (isset($_GET['referendum'])) {
   $referendum = $mysqli->escape_string($_GET['referendum']);
   $referendum = str_replace(' ', '+', $referendum);
-  $query = "SELECT url FROM trustee LEFT JOIN referendum ON referendum.trustee = trustee.`key` LEFT JOIN publication " .
+  $query = "SELECT url FROM judge LEFT JOIN referendum ON referendum.judge = judge.`key` LEFT JOIN publication " .
            "ON publication.id = referendum.id WHERE publication.`key` = \"$referendum\"";
 } else if (isset($_GET['key'])) {
   $key = $mysqli->escape_string($_GET['key']);
   $key = str_replace(' ', '+', $key);
-  $query = "SELECT url FROM trustee WHERE `key` = \"$key\"";
+  $query = "SELECT url FROM judge WHERE `key` = \"$key\"";
 } else
   die("Missing key or referendum argument.");
 $result = $mysqli->query($query);
 if (!$result)
-  die("Trustee not found.");
-$trustee = $result->fetch_assoc();
+  die("Judge not found.");
+$judge = $result->fetch_assoc();
 $result->free();
-die($trustee['url']);
+die($judge['url']);
 ?>
