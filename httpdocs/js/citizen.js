4ea23c6992b741d8af13a8498161696ad575502e
megatol <74714212+megatol@users.noreply.github.com>
2023-08-18 08:34:10 +0200

    Update citizen.js

--- httpdocs/js/citizen.js
+++ httpdocs/js/citizen.js
@@ -7,7 +7,7 @@ function findGetParameter(parameterName, result = null) {
 }
 
 window.onload = function() {
-  let trustee = findGetParameter('trustee', 'https://trustee.directdemocracy.vote');
+  let judge = findGetParameter('judge', 'https://trustee.directdemocracy.vote');
   const fingerprint = findGetParameter('fingerprint');
   if (!fingerprint) {
     console.log('Missing fingerprint GET argument.');
@@ -95,22 +95,22 @@ window.onload = function() {
         `<h4>Reputation: <span id="reputation">...</span></h4>
 <div class="input-group mb-3">
   <span class="input-group-text" id="protocol">https://</span>
-  <input id="trustee" type="text" class="form-control" placeholder="https://trustee.directdemocracy.vote"
-   aria-label="Trustee URL" aria-describedby="protocol" value="${trustee.substring(8)}">
+  <input id="judge" type="text" class="form-control" placeholder="https://trustee.directdemocracy.vote"
+   aria-label="Judge URL" aria-describedby="protocol" value="${judge.substring(8)}">
    <button class="btn btn-outline-secondary" type="button" id="reload"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
   <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
   <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
 </svg></button>
 </div>`;
       document.getElementById('reload').addEventListener('click', function(event) {
-        trustee = 'https://' + document.getElementById('trustee').value;
+        judge = 'https://' + document.getElementById('judge').value;
         document.getElementById('reputation').innerHTML = '...';
         loadReputation();
       });
 
       function loadReputation() {
-        const url = trustee + '/reputation.php?key=' + encodeURIComponent(answer.citizen.key);
-        xhttp = new XMLHttpRequest(); // get reputation from trustee
+        const url = judge + '/reputation.php?key=' + encodeURIComponent(answer.citizen.key);
+        xhttp = new XMLHttpRequest(); // get reputation from judge
         xhttp.open('GET', url, true);
         xhttp.send();
         xhttp.onload = function() {
