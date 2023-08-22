bbf28a8965258c7d9e5389282e0776825b854f76
megatol <74714212+megatol@users.noreply.github.com>
2023-08-22 14:54:29 +0200

    Update notary.js

--- httpdocs/js/notary.js
+++ httpdocs/js/notary.js
@@ -32,28 +32,12 @@ window.onload = function() {
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
   });
-  let marker = L.marker([latitude, longitude]).addTo(map).bindPopup(latitude + ',' + longitude).on('click', onMarkerClick);
+  let marker = L.marker([latitude, longitude]).addTo(map).bindPopup(latitude + ',' + longitude).on('click', updateLabel);
   let circle = L.circle([latitude, longitude], { color: 'red', opacity: 0.4, fillColor: '#f03', fillOpacity: 0.2, radius: range}).addTo(map);
   marker.setPopupContent(`<div style="text-align:center" id="address">${address}</div><div><input type="range" min="5" max="100" value="${slider}" class="slider" id="range"></div>` +
     `<div style="text-align:center;color:#999" id="position">(${latitude}, ${longitude} &plusmn; ${Math.round(range / 100) / 10} km</div></center>`).openPopup();
   document.getElementById('range').addEventListener('input', rangeChanged);
-  map.on('click', onMapClick);
-  map.on('contextmenu', function(event) { return false; });
-  updatePosition();
-
-  function getGeolocationPosition(position) {
-    geolocation = true;
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    map.setView([position.coords.latitude, position.coords.longitude], 12);
-    setTimeout(updatePosition, 500);
-  }
-  
-  function onMarkerClick(e) {
-    updateLabel();
-  }
-  
-  function onMapClick(e) {
+  map.on('click', function(e) {
     marker.setLatLng(e.latlng).openPopup();
     circle.setLatLng(e.latlng);
     latitude = e.latlng.lat;
@@ -63,6 +47,43 @@ window.onload = function() {
     let range = document.getElementById('range');
     range.setAttribute('value', slider);
     range.addEventListener('input', rangeChanged);
+  });
+  map.on('contextmenu', function(event) { return false; });
+  updatePosition();
+  document.getElementById('search-ciziten').addEventListener('click', function() {
+    const familyName = document.getElementById("family-name").value;
+    const givenNames = document.getElementById("given-names").value;
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        const a = JSON.parse(this.responseText);
+        markers.forEach(function(m) { // delete previous markers
+          map.removeLayer(m);
+        });
+        markers = [];
+        a.forEach(function(c) {
+          const name = c.givenNames + ' ' + c.familyName;
+          const fingerprint = CryptoJS.SHA1(c.signature).toString();
+          const label = '<div style="text-align:center"><a target="_blank" href="/citizen.html?fingerprint=' + fingerprint + '"><img src="' + c.picture + '" width="60" height="80"><br>' + name + '</a></div>';
+          markers.push(L.marker([c.latitude, c.longitude], {
+            icon: greenIcon
+          }).addTo(map).bindPopup(label));
+        });
+      }
+    }
+    let parameters = "latitude=" + latitude + "&longitude=" + longitude + "&range=" + range;
+    if (familyName) parameters += "&familyName=" + encodeURI(familyName);
+    if (givenNames) parameters += "&givenNames=" + encodeURI(givenNames);
+    xhttp.open("GET", "https://notary.directdemocracy.vote/api/search.php?" + parameters, true);
+    xhttp.send();
+  });
+
+  function getGeolocationPosition(position) {
+    geolocation = true;
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    map.setView([position.coords.latitude, position.coords.longitude], 12);
+    setTimeout(updatePosition, 500);
   }
   
   function rangeChanged(e) {
@@ -90,34 +111,6 @@ window.onload = function() {
   function updateLabel() {
     document.getElementById("address").innerHTML = address;
     document.getElementById("position").innerHTML = '(' + latitude + ', ' + longitude + ') &plusmn; ' + Math.round(range / 100) / 10 + ' km';
-  }
-  
-  function search() {
-    const familyName = document.getElementById("family-name").value;
-    const givenNames = document.getElementById("given-names").value;
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        const a = JSON.parse(this.responseText);
-        markers.forEach(function(m) { // delete previous markers
-          map.removeLayer(m);
-        });
-        markers = [];
-        a.forEach(function(c) {
-          const name = c.givenNames + ' ' + c.familyName;
-          const fingerprint = CryptoJS.SHA1(c.signature).toString();
-          const label = '<div style="text-align:center"><a target="_blank" href="/citizen.html?fingerprint=' + fingerprint + '"><img src="' + c.picture + '" width="60" height="80"><br>' + name + '</a></div>';
-          markers.push(L.marker([c.latitude, c.longitude], {
-            icon: greenIcon
-          }).addTo(map).bindPopup(label));
-        });
-      }
-    }
-    let parameters = "latitude=" + latitude + "&longitude=" + longitude + "&range=" + range;
-    if (familyName) parameters += "&familyName=" + encodeURI(familyName);
-    if (givenNames) parameters += "&givenNames=" + encodeURI(givenNames);
-    xhttp.open("GET", "https://notary.directdemocracy.vote/api/search.php?" + parameters, true);
-    xhttp.send();
   }
 }
 
