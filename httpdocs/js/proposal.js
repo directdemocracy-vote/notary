a8ec526329e320a2b55015efbd8adb16c4856904
megatol <74714212+megatol@users.noreply.github.com>
2023-08-23 17:01:27 +0200

    Update proposal.js

--- httpdocs/js/proposal.js
+++ httpdocs/js/proposal.js
@@ -50,11 +50,10 @@ window.onload = function() {
   generateCryptographicKey();
 
   function updateArea() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        const a = JSON.parse(this.responseText);
-        let address = a.address;
+    fetch(`https://nominatim.openstreetmap.org/reverse.php?format=json&lat=${latitude}&lon={longitude}&zoom=10`)
+      .then((response) => response.json())
+      .then((answer) => {
+        let address = answer.address;
         let select = document.getElementById('area');
         let count = 0;
 
@@ -62,31 +61,16 @@ window.onload = function() {
           if (level in address)
             select.options[count++] = new Option(address[level], level);
         }
-        // we ignore admin levels lowed than 'village':
-        // 'block', 'neighbourhood', 'quarter', 'suburb', 'borough' and 'hamlet'
-        const admin = ['village', 'town',
-          'city',
-          'municipality',
-          'county', 'district',
-          'region', 'province', 'state',
-          'country'
-        ];
-        admin.forEach(function(item) {
-          addAdminLevel(item);
-        });
+        // we ignore admin levels lowed than 'village': 'block', 'neighbourhood', 'quarter', 'suburb', 'borough' and 'hamlet'
+        const admin = ['village', 'town', 'city', 'municipality', 'county', 'district', 'region', 'province', 'state', 'country'];
+        admin.forEach(function(item) { addAdminLevel(item); });
         const country_code = address.country_code.toUpperCase();
         if (['GB', 'DE', 'FR', 'IT', 'SE', 'PL', 'RO', 'HR', 'ES', 'NL', 'IE', 'BG', 'DK', 'GR',
-            'AT', 'HU', 'FI', 'CZ', 'PT', 'BE', 'MT', 'CY', 'LU', 'SI', 'LU', 'SK', 'EE', 'LV'
-          ]
-          .indexOf(country_code) >= 0)
+            'AT', 'HU', 'FI', 'CZ', 'PT', 'BE', 'MT', 'CY', 'LU', 'SI', 'LU', 'SK', 'EE', 'LV'].indexOf(country_code) >= 0)
           select.options[count++] = new Option('European Union', 'union');
         select.options[count++] = new Option('Earth', 'world');
         areaChange();
-      }
-    };
-    xhttp.open('GET', 'https://nominatim.openstreetmap.org/reverse.php?format=json&lat=' + latitude + '&lon=' + longitude +
-      '&zoom=10', true);
-    xhttp.send();
+      });
   }
 
   function areaChange() {
