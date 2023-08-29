6e819f7a2b707e429d494fea6dfbcf752fa8a8f9
megatol <74714212+megatol@users.noreply.github.com>
2023-08-29 12:21:18 +0200

    f

--- httpdocs/api/search.php
+++ httpdocs/api/search.php
@@ -58,7 +58,8 @@ $query .= ", publication.`schema`, publication.`key`, publication.signature, pub
 $query .= " FROM citizen";
 $query .= " INNER JOIN publication ON publication.id = citizen.id";
 if ($key)
-  $query .= " INNER JOIN endorsement ON endorsement.endorsedFingerprint = publication.fingerprint AND publication.`key` = '$key' AND endorsement.`revoke` = 0 AND endorsement.latest = 1";
+  $query .= " INNER JOIN endorsement ON endorsement.endorsedFingerprint = publication.fingerprint AND endorsement.`revoke` = 0 AND endorsement.latest = 1"
+           ." INNER JOIN publication AS pe ON pe.id=endorsement.id AND pe.`key` = '$key' ";
 if ($familyName or $givenNames) {
   $query .= " WHERE";
   if ($familyName) {
