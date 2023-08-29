c7a98bacc958cbb19d7a97a54dc21736dfd48a54
megatol <74714212+megatol@users.noreply.github.com>
2023-08-29 11:52:25 +0200

    f

--- httpdocs/api/search.php
+++ httpdocs/api/search.php
@@ -44,8 +44,8 @@ $judge = $mysqli->escape_string(get_string_parameter('judge'));
 
 $key = '';
 if ($judge) {
-  $caca = "SELECT `key` FROM judge WHERE url='https://$judge'";
-  $result = $mysqli->query("SELECT `key` FROM judge WHERE url='https://$judge'") or error($mysqli->error);
+  $caca = "SELECT `key` FROM judge WHERE url='$judge'";
+  $result = $mysqli->query("SELECT `key` FROM judge WHERE url='$judge'") or error($mysqli->error);
   if ($j = $result->fetch_assoc())
     $key = $j['key'];
   $result->free();
