fd5105f0f9546f75f7baa6ce3553fb34867e2d36
megatol <74714212+megatol@users.noreply.github.com>
2023-08-29 11:45:22 +0200

    Use endorsement.latest

--- php/endorsements.php
+++ php/endorsements.php
@@ -1,23 +1,18 @@
 <?php
 function endorsements($mysqli, $key) {
-  $query = "SELECT pc.id, pc.fingerprint, pe.published, e.`revoke`, pc.`signature`, "
+  $query = "SELECT pc.fingerprint, pe.published, e.`revoke`, pc.`signature`, "
           ."c.familyName, c.givenNames, c.picture "
           ."FROM publication pe "
           ."INNER JOIN endorsement e ON e.id = pe.id "
           ."INNER JOIN publication pc ON pc.`signature` = e.endorsedSignature "
           ."INNER JOIN citizen c ON pc.id = c.id "
-          ."WHERE pe.`key` = '$key' "
+          ."WHERE pe.`key` = '$key' AND e.latest = 1 "
           ."ORDER BY pe.published DESC";
   $result = $mysqli->query($query);
   if (!$result)
     return array('error' => $mysqli->error);
   $endorsements = array();
-  $already = array();
   while($e = $result->fetch_assoc()) {
-    if (in_array($e['id'], $already))
-      continue;
-    $already[] = $e['id'];
-    unset($e['id']);
     settype($e['published'], 'int');
     $e['revoke'] = (intval($e['revoke']) == 1);
     settype($e['latitude'], 'float');
