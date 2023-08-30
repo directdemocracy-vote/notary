560f0045934daf40bd38a67996a17038e0eb8262
megatol <74714212+megatol@users.noreply.github.com>
2023-08-30 13:45:34 +0200

    done corpus

--- php/corpus.php
+++ php/corpus.php
@@ -1,6 +1,7 @@
 <?php
 function update_corpus($mysqli, $id) {
-  $query = "SELECT COUNT(citizen.id) FROM citizen "
+  $query = "UPDATE proposal SET corpus = "
+          "(SELECT COUNT(citizen.id) FROM citizen "
           ."INNER JOIN publication AS pc ON pc.id=citizen.id "
           ."INNER JOIN endorsement ON endorsement.endorsedFingerprint=pc.fingerprint "
           ."INNER JOIN publication AS pe ON pe.id=endorsement.id "
@@ -9,7 +10,8 @@ function update_corpus($mysqli, $id) {
           ."INNER JOIN area ON area.id=pa.id "
           ."INNER JOIN judge ON judge.url = proposal.judge "
           ."WHERE endorsement.latest=1 AND endorsement.`revoke`=0 AND pe.`key`=judge.`key` "
-          ."AND ST_Contains(area.polygons, POINT(ST_X(citizen.home), ST_Y(citizen.home)))";
+          ."AND ST_Contains(area.polygons, POINT(ST_X(citizen.home), ST_Y(citizen.home))))";
+  $mysqli->query($query) or die($mysqli->error);
   return;
 }
 ?>
