2037a70594f6fafab894abcd8cd56c96e879f98a
megatol <74714212+megatol@users.noreply.github.com>
2023-08-30 17:46:50 +0200

    a

--- httpdocs/private/proposal.php
+++ httpdocs/private/proposal.php
@@ -3,12 +3,6 @@
 
 require_once '../../php/database.php';
 
-function error($message) {
-  if ($message[0] != '{')
-    $message = '"'.$message.'"';
-  die("{\"error\":$message}");
-}
-
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
@@ -19,22 +13,7 @@ if ($mysqli->connect_errno)
 $mysqli->set_charset('utf8mb4');
 $fingerprint = $mysqli->escape_string($_GET['fingerprint']);
 if (!isset($fingerprint))
-  error('Missing fingerprint parameter.');
-
-function return_results($query) {
-  global $mysqli;
-  $result = $mysqli->query($query) or die($mysqli->error);
-  $proposals = array();
-  while ($proposal = $result->fetch_assoc()) {
-    $proposal['participation'] = 0;
-    $proposal['corpus'] = 0;
-    set_types($proposal);
-    $proposals[] = $proposal;
-  }
-  $result->free();
-  $mysqli->close();
-  die(json_encode($proposals, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
-}
+  die('Missing fingerprint parameter.');
 
 $query = "SELECT "
         ."publication.schema, publication.key, publication.signature, publication.published, "
@@ -47,21 +26,20 @@ $query = "SELECT "
         ."LEFT JOIN publication AS pa ON pa.signature = proposal.area "
         ."LEFT JOIN area ON area.id = pa.id "
         ."WHERE publication.fingerprint = '$fingerprint'";
-  $result = $mysqli->query($query) or die($mysqli->error);
-  $proposal = $result->fetch_assoc();
-  $result->free();
-  $mysqli->close();
-  $polygons = json_decode($proposal['polygons']);
-  if ($polygons->type !== 'MultiPolygon')
-    die("Area without MultiPolygon: $polygons->type");
-  $proposal['polygons'] = &$polygons->coordinates;
-  $proposal['name'] = explode("\n", $proposal['areas']);
-  settype($proposal['published'], 'int');
-  settype($proposal['secret'], 'bool');
-  settype($proposal['deadline'], 'int');
-  settype($proposal['participation'], 'int');
-  settype($proposal['corpus'], 'int');
-  $json = json_encode($proposal, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-  die($json);
-}
+$result = $mysqli->query($query) or die($mysqli->error);
+$proposal = $result->fetch_assoc();
+$result->free();
+$mysqli->close();
+$polygons = json_decode($proposal['polygons']);
+if ($polygons->type !== 'MultiPolygon')
+  die("Area without MultiPolygon: $polygons->type");
+$proposal['polygons'] = &$polygons->coordinates;
+$proposal['name'] = explode("\n", $proposal['areas']);
+settype($proposal['published'], 'int');
+settype($proposal['secret'], 'bool');
+settype($proposal['deadline'], 'int');
+settype($proposal['participation'], 'int');
+settype($proposal['corpus'], 'int');
+$json = json_encode($proposal, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+die($json);
 ?>
