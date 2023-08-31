c549eb0714a92d863f1167eccae716c57a701a83
megatol <74714212+megatol@users.noreply.github.com>
2023-08-31 21:22:18 +0200

    f

--- httpdocs/api/publish.php
+++ httpdocs/api/publish.php
@@ -143,12 +143,12 @@ elseif ($type == 'endorsement') {
     $endorsed_id = $endorsed['id'];
     $key = endorsement->key;
     $query = "UPDATE proposal SET participants=participants+1 "
-            ."INNER JOIN publication AS pp ON pp.id = proposal.id "
-            ."INNER JOIN publication AS PC ON pc.`key`='$key' "
+            ."INNER JOIN publication AS pc ON pc.`key`='$key' "
             ."INNER JOIN citizen ON citizen.id=pc.id "
             ."INNER JOIN publication AS pa ON pa.`signature`=proposal.area "
             ."INNER JOIN area ON on area.id=pa.id AND ST_Contains(area.polygons, POINT(ST_X(citizen.home), ST_Y(citizen.home))) "
-            ."INNER JOIN publication AS pe ON pe.`key`=pp.`key` "
+            ."INNER JOIN judge ON judge.url=proposal.judge "
+            ."INNER JOIN publication AS pe ON pe.`key`=judge.`key` "
             ."INNER JOIN endorsement ON endorsement.id = pe.id AND endorsement.`revoke`=0 AND endorsement.latest=1 AND endorsement.endorsedFingerprint=pc.fingerprint "
             ."WHERE proposal.id=$endorsed_id AND proposal.`secret`=0 ";
     $mysqli->query($query) or error($msqli->error); 
