29ace5f100d3372d36a003a86dc53d0d82d65430
megatol <74714212+megatol@users.noreply.github.com>
2023-08-31 20:27:32 +0200

    a

--- httpdocs/api/participants.php
+++ httpdocs/api/participants.php
@@ -39,7 +39,7 @@ $query = "SELECT pc.fingerprint, citizen.firstName, citizen.lastName, citizen.pi
         ."INNER JOIN pe ON pe.`key`=judge.`key` "
         ."INNER JOIN endorsement ON endorsement.id=pe.id AND endorsement.latest=1 AND endorsement.`revoke`=0 AND endorsement.endorsedFingerprint=pc.fingerprint "
         ."INNER JOIN publication AS pa ON proposal.area=pa.`key` "
-        ."INNER JOIN area ON area.id=pa.id AND ST_Contains(area.polygons, POINT(ST_X(citizen.home), ST_Y(citizen.home)))) ";
+        ."INNER JOIN area ON area.id=pa.id AND ST_Contains(area.polygons, POINT(ST_X(citizen.home), ST_Y(citizen.home))) ";
 $result = $mysqli->query($query) or error($query . " - " . $mysqli->error);
 $participants = array();
 while ($participant = $result->fetch_assoc())
