8e9af1908d91910038a6ee588262f4193ebe2c30
megatol <74714212+megatol@users.noreply.github.com>
2023-09-01 12:30:11 +0200

    date

--- httpdocs/api/participants.php
+++ httpdocs/api/participants.php
@@ -35,20 +35,22 @@ if (!$title)
   error("Proposal not found");
 $answer = array();
 $answer['title'] = $title['title'];
-$query = "SELECT pc.fingerprint, citizen.givenNames, citizen.familyName, citizen.picture "
-        ."FROM citizen "
-        ."INNER JOIN publication AS pc ON pc.id=citizen.id "
-        ."INNER JOIN publication AS pp ON pp.fingerprint='$fingerprint' "
-        ."INNER JOIN proposal ON proposal.id=pp.id "
-        ."INNER JOIN judge ON judge.url=proposal.judge "
-        ."INNER JOIN publication AS pe ON pe.`key`=judge.`key` "
-        ."INNER JOIN endorsement ON endorsement.id=pe.id AND endorsement.latest=1 AND endorsement.`revoke`=0 AND endorsement.endorsedFingerprint=pc.fingerprint "
-        ."INNER JOIN publication AS pa ON proposal.area=pa.`signature` "
-        ."INNER JOIN area ON area.id=pa.id AND ST_Contains(area.polygons, POINT(ST_X(citizen.home), ST_Y(citizen.home))) ";
+$query = "SELECT pc.fingerprint, citizen.givenNames, citizen.familyName, citizen.picture";
 if (!$corpus)
-  $query .= "INNER JOIN endorsement AS signature ON signature.endorsedFingerprint='$fingerprint' "
-           ."INNER JOIN publication AS ps ON ps.id=signature.id AND ps.`key`=pc.`key` ";
-$query .= "ORDER BY citizen.familyName, citizen.givenNames";
+  $query .= ", ps.published";
+$query .= " FROM citizen"
+         ." INNER JOIN publication AS pc ON pc.id=citizen.id"
+         ." INNER JOIN publication AS pp ON pp.fingerprint='$fingerprint'"
+         ." INNER JOIN proposal ON proposal.id=pp.id"
+         ." INNER JOIN judge ON judge.url=proposal.judge"
+         ." INNER JOIN publication AS pe ON pe.`key`=judge.`key`"
+         ." INNER JOIN endorsement ON endorsement.id=pe.id AND endorsement.latest=1 AND endorsement.`revoke`=0 AND endorsement.endorsedFingerprint=pc.fingerprint"
+         ." INNER JOIN publication AS pa ON proposal.area=pa.`signature`"
+         ." INNER JOIN area ON area.id=pa.id AND ST_Contains(area.polygons, POINT(ST_X(citizen.home), ST_Y(citizen.home)))";
+if (!$corpus)
+  $query .= " INNER JOIN endorsement AS signature ON signature.endorsedFingerprint='$fingerprint'"
+           ." INNER JOIN publication AS ps ON ps.id=signature.id AND ps.`key`=pc.`key`";
+$query .= " ORDER BY citizen.familyName, citizen.givenNames";
 
 $result = $mysqli->query($query) or error($query . " - " . $mysqli->error);
 $participants = array();
