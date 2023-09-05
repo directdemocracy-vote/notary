89c5583c83af301f7485b0e09362a92f625bf11f
megatol <74714212+megatol@users.noreply.github.com>
2023-09-05 11:04:06 +0200

    d

--- httpdocs/js/proposal.js
+++ httpdocs/js/proposal.js
@@ -25,7 +25,7 @@ window.onload = function() {
       document.getElementById('title').innerHTML = answer.title;
       document.getElementById('description').innerHTML = answer.description;
       if (answer.secret) {
-        document.getElementById('question').innerHTML = answer.question;
+        // document.getElementById('question').innerHTML = answer.question;
         // document.getElementById('answers').innerHTML = 
       }
       const deadline = new Date(answer.deadline);
