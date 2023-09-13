9442f93bc5fa326801febb30be574903528b635f
megatol <74714212+megatol@users.noreply.github.com>
2023-09-13 15:39:09 +0200

    f

--- httpdocs/api/participation.php
+++ httpdocs/api/participation.php
@@ -15,6 +15,15 @@ function get_string_parameter($name) {
   return FALSE;
 }
 
+function public_key($key) {
+  $public_key = "-----BEGIN PUBLIC KEY-----\n";
+  $l = strlen($key);
+  for($i = 0; $i < $l; $i += 64)
+    $public_key .= substr($key, $i, 64) . "\n";
+  $public_key.= "-----END PUBLIC KEY-----";
+  return $public_key;
+}
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
