8005161894350a6124d01385220e2c92d5cdc776
megatol <74714212+megatol@users.noreply.github.com>
2023-09-20 17:19:07 +0200

    d

--- httpdocs/api/participation.php
+++ httpdocs/api/participation.php
@@ -59,13 +59,6 @@ $result = $mysqli->query($query) or error($mysqli->error);
 $publication = $result->fetch_assoc();
 $result->free();
 if (!$publication) {
-
-  
-  
-  die("$station/api/participation.php?referendum=" . urlencode($referendumKey));
-
-
-
   $answer = file_get_contents("$station/api/participation.php?referendum=" . urlencode($referendumKey));
   $publication = json_decode($answer,true);
   $signature = $publication['signature'];
