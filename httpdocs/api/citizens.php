70239e2a0753e2609f72f28805dd22daebb2e9ba
megatol <74714212+megatol@users.noreply.github.com>
2023-09-27 12:31:18 +0200

    f

--- httpdocs/api/citizens.php
+++ httpdocs/api/citizens.php
@@ -73,6 +73,7 @@ if ($familyName or $givenNames) {
 if ($radius)
   $query .= " HAVING distance < $radius ORDER BY distance";
 $query .= " LIMIT 0, 20;";
+die($query);
 $result = $mysqli->query($query) or die($mysqli->error);
 $citizens = array();
 while ($citizen = $result->fetch_assoc()) {
