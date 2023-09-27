bd88cbc5cddbaff25c59a378cc253b3dd82edc3c
megatol <74714212+megatol@users.noreply.github.com>
2023-09-27 12:11:45 +0200

    d

--- httpdocs/js/citizen.js
+++ httpdocs/js/citizen.js
@@ -116,10 +116,14 @@ window.onload = function() {
         button.classList.remove('is-loading');
       }
 
+      function deg2rad(deg) {
+        return deg * Math.PI / 180;
+      }
+
       function distanceFromLatitudeLongitude(lat1, lon1, lat2, lon2) {
         const R = 6371000; // Radius of the Earth in m
-        const dLat = (lat2 - lat1) * Math.PI / 180;
-        const dLon = (lon2 - lon1) * Math.PI / 180; 
+        const dLat = deg2rad(lat2 - lat1);
+        const dLon = deg2rad(lon2 - lon1); 
         const a =
           Math.sin(dLat / 2) * Math.sin(dLat / 2) +
           Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
