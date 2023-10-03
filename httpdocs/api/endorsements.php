8439b5395d1dbc2c848fc018c6e68557447fb27c
megatol <74714212+megatol@users.noreply.github.com>
2023-10-03 12:40:59 +0200

    fix

--- httpdocs/api/endorsements.php
+++ httpdocs/api/endorsements.php
@@ -11,16 +11,26 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
 
-if (!isset($_GET['fingerprint']))
-  error("Missing fingerprint parameter");
-if (!isset($_GET['judge']))
+
+if (isset($_GET['fingerprint'])) {
+  $fingerprint = $mysqli->escape_string($_GET['fingerprint']);
+  $condition = "publication.signatureSHA1=UNHEX('$fingerprint')";
+  $join_condition = "SHA1(endorsement.endorsedSignature)='$fingerprint'";
+} elseif (isset(_GET['signature'])) {
+  $signature = $mysqli->escape_string($(_GET['signature']));
+  $condition = "publication.signature=FROM_BASE64('$signature')";
+  $join_condition = "endorsement.endorsedSignature=FROM_BASE64('$signature')";
+} else
+  error("Missing fingerprint or signature parameter");
+
+if (isset($_GET['judge']))
+  $judge = $mysqli->escape_string($_GET['judge']);
+else
   error("Missing judge parameter");
 
-$fingerprint = $mysqli->escape_string($_GET['fingerprint']);
-$judge = $mysqli->escape_string($_GET['judge']);
 
 $query = "SELECT citizen.givenNames, citizen.familyName FROM citizen INNER JOIN publication ON publication.id=citizen.id "
-        ."AND publication.signatureSHA1 = UNHEX('$fingerprint')";
+        ."AND $condition";
 $result = $mysqli->query($query) or error($mysqli->error);
 $citizen = $result->fetch_assoc();
 $result->free();
@@ -29,7 +39,7 @@ $answer['givenNames'] = $citizen['givenNames'];
 $answer['familyName'] = $citizen['familyName'];
 $query = "SELECT UNIX_TIMESTAMP(publication.published) AS published, endorsement.`revoke`, endorsement.latest FROM publication"
         ." INNER JOIN webservice AS judge ON judge.`type`='judge' AND judge.`key`=publication.`key` AND judge.url='$judge'"
-        ." INNER JOIN endorsement ON endorsement.id=publication.id AND SHA1(endorsement.endorsedSignature)='$fingerprint'"
+        ." INNER JOIN endorsement ON endorsement.id=publication.id AND $join_condition"
         ." ORDER BY publication.published DESC";
 $result = $mysqli->query($query) or error($mysqli->error);
 $endorsements = array();
