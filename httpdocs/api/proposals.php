d111caf2dc836e742adbd77543a1eac969c42d0e
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-10-05 08:50:17 +0300

    debug

--- httpdocs/api/proposals.php
+++ httpdocs/api/proposals.php
@@ -62,12 +62,14 @@ if ($secret == 2)
   $secret = '';
 else # assuming 0 or 1
   $secret = "proposal.secret = $secret AND ";
+
+die($open);
 if ($open == 2)
   $open = '';
 elseif ($open == 0)
-  $open = "proposal.deadline * 1000 <= NOW() AND ";
+  $open = "proposal.deadline <= NOW() AND ";
 else # assuming 1
-  $open = "proposal.deadline * 1000 > NOW() AND ";
+  $open = "proposal.deadline > NOW() AND ";
 if ($search !== '')
   $search = "(title LIKE \"%$search%\" OR description LIKE \"%$search%\") AND ";
 $query = "SELECT "
