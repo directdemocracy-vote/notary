95e9ba96f3c30c6a170227b02de23935b6017b35
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-10-06 08:25:10 +0200

    sanitize test

--- httpdocs/js/notary.js
+++ httpdocs/js/notary.js
@@ -150,7 +150,8 @@ window.onload = function() {
     const searchProposal = document.getElementById('search-proposals');
     searchProposal.classList.add('is-loading');
     const query = document.getElementById('proposal-query').value;
-    console.log(query)
+    const sanitizer = new Sanitizer();
+    console.log(sanitizer.sanitize(query));
     const r = document.getElementById('proposal-referendum').checked;
     const p = document.getElementById('proposal-petition').checked;
     const secret = (r && p) ? 2 : (r ? 1 : 0);
