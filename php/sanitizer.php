9be1a1c3169622715e9147962276081d3078b0a4
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-10-06 10:00:56 +0200

    test

--- php/sanitizer.php
+++ php/sanitizer.php
@@ -82,6 +82,7 @@ function sanitize_string($variable, $name) {
   if (!is_string($variable))
     error("Error: $name should be a string.");
 
+  die($variable)
   $blacklistedChars = '"\'<>&';
   $pattern = preg_quote($blacklistedChars, '/');
   if (preg_match('/[' . $pattern . ']/', $variable))
