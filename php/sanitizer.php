fcb8cbd78ab8142e7c5ca2bc1465a1f93c69e85d
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-10-06 15:43:01 +0200

    test

--- php/sanitizer.php
+++ php/sanitizer.php
@@ -86,7 +86,7 @@ function sanitize_string($variable, $name) {
   $pattern = preg_quote($blacklistedChars, '/');
 
   if (preg_match('/[' . $pattern . ']/', $variable))
-     error("$name contains non escaped characters.");
+     error("$name contains non escaped characters. $variable");
 
   //Check for &
   $test_variable = str_replace("&amp;", "", $variable);
