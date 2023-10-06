143e5a693c466bf35cd81436c030017147426d50
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-10-06 13:54:24 +0200

    test

--- httpdocs/js/proposal.js
+++ httpdocs/js/proposal.js
@@ -26,7 +26,7 @@ window.onload = function() {
         return;
       }
       document.getElementById('title').innerText = answer.title;
-      document.getElementById('description').innerHTML = answer.description;
+      document.getElementById('description').textContent = answer.description;
       if (answer.secret) {
         document.getElementById('question-block').style.display = '';
         document.getElementById('question').textContent = answer.question;
