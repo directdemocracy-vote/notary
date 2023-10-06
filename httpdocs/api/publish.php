007f6c020324988de2c83cb71fb1311fdfd84a98
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-10-06 12:39:55 +0200

    missing escap

--- httpdocs/api/publish.php
+++ httpdocs/api/publish.php
@@ -33,7 +33,7 @@ if (!$publication)
   error("Unable to parse JSON post");
 if (!isset($publication->schema))
   error("Unable to read schema field");
-$schema = sanitize_field($publication->schema, 'string', 'schema');
+$schema = sanitize_field($publication->schema, "string", "schema");
 $key = sanitize_field($publication->key, "base_64", "key");
 $published = sanitize_field($publication->published, 'positive_int', 'published');
 $signature = sanitize_field($publication->signature, "base_64", "signature");
@@ -56,6 +56,7 @@ if ($type != 'ballot' && $published > $now + 60)  # allowing a 1 minute (60 seco
 if ($type == 'citizen') {
   $citizen = &$publication;
   $citizen_picture = substr($citizen->picture, strlen('data:image/jpeg;base64,'));
+  $citizen_picture = sanitize_field($citizen_picture, "base_64", "citizen_picture");
   $data = base64_decode($citizen_picture);
   try {
     $size = @getimagesizefromstring($data);
@@ -88,11 +89,15 @@ $query = "INSERT INTO publication(`version`, `type`, `key`, signature, published
 $mysqli->query($query) or error($mysqli->error);
 $id = $mysqli->insert_id;
 
-if ($type == 'citizen')
+if ($type == 'citizen') {
+  $familyName = sanitize_field($publication->familyName, 'string', 'familyName');
+  $givenNames = sanitize_field($publication->givenNames, 'string', 'givenNames');
+  $latitude = sanitize_field($citizen->latitude, 'float', 'latitude');
+  $longitude = sanitize_field($citizen->longitude, 'float', 'longitude');
   $query = "INSERT INTO citizen(id, familyName, givenNames, picture, home) "
-          ."VALUES($id, \"$citizen->familyName\", \"$citizen->givenNames\", "
-          ."FROM_BASE64(\"$citizen_picture\"), POINT($citizen->longitude, $citizen->latitude))";
-elseif ($type == 'endorsement') {
+          ."VALUES($id, \"$familyName\", \"$givenNames\", "
+          ."FROM_BASE64(\"$citizen_picture\"), POINT($longitude, $latitude))";
+} elseif ($type == 'endorsement') {
   $endorsement = &$publication;
   if (!property_exists($endorsement, 'revoke'))
     $endorsement->revoke = false;
@@ -100,18 +105,19 @@ elseif ($type == 'endorsement') {
     $endorsement->message = '';
   if (!property_exists($endorsement, 'comment'))
     $endorsement->comment = '';
-  $query = "SELECT id, `type`, REPLACE(TO_BASE64(signature), '\\n', '') AS signature FROM publication WHERE signature = FROM_BASE64('$endorsement->endorsedSignature')";
+  $endorsedSignature = sanitize_field($endorsement->endorsedSignature, "base_64", "endorsedSignature");
+  $query = "SELECT id, `type`, REPLACE(TO_BASE64(signature), '\\n', '') AS signature FROM publication WHERE signature = FROM_BASE64('$endorsedSignature')";
   $result = $mysqli->query($query) or error($mysqli->error);
   $endorsed = $result->fetch_assoc();
   $result->free();
   if (!$endorsed)
-    error("Endorsed signature not found: $endorsement->endorsedSignature");
-  if ($endorsed['signature'] != $endorsement->endorsedSignature)
+    error("Endorsed signature not found: $endorsedSignature");
+  if ($endorsed['signature'] != $endorsedSignature)
     error("Endorsed signature mismatch.");
   # mark other endorsements of the same participant by the same endorser as not the latest
   $mysqli->query("UPDATE endorsement INNER JOIN publication ON publication.id = endorsement.id"
                 ." SET endorsement.latest = 0"
-                ." WHERE endorsement.endorsedSignature = FROM_BASE64('$endorsement->endorsedSignature')"
+                ." WHERE endorsement.endorsedSignature = FROM_BASE64('$endorsedSignature')"
                 ." AND publication.`key` = FROM_BASE64('$key')") or error($mysli->error);
   if ($endorsed['type'] == 'proposal') {  # signing a petition
     # increment the number of participants in a petition if the citizen is located inside the petition area and is endorsed by the petition judge
@@ -131,8 +137,10 @@ elseif ($type == 'endorsement') {
   } else
     $accepted = 0;
   $revoke = $endorsement->revoke ? 1 : 0;
+  $message = sanitize_field($endorsement->message, "string", "message");
+  $comment = sanitize_field($endorsement->comment, "string", "comment");
   $query = "INSERT INTO endorsement(id, `revoke`, `message`, comment, endorsedSignature, latest, accepted) "
-          ."VALUES($id, $revoke, \"$endorsement->message\", \"$endorsement->comment\", FROM_BASE64('$endorsement->endorsedSignature'), 1, $accepted)";
+          ."VALUES($id, $revoke, \"$message\", \"$comment\", FROM_BASE64('$endorsedSignature'), 1, $accepted)";
 } elseif ($type == 'proposal') {
   $proposal =&$publication;
   if (!isset($proposal->website))  # optional
@@ -208,7 +216,7 @@ elseif ($type == 'ballot') {
   $polygons .= ')")';
   $polygons = sanitize_field($publication->polygons, 'string', 'polygons');
   $name = sanitize_field($publication->name, 'string', 'name');
-  $name = implode("\n", $publication->name);
+  $name = implode("\n", $name);
   $query = "INSERT INTO area(id, name, polygons) VALUES($id, \"$name\", $polygons)";
 } else
   error("Unknown publication type.");
