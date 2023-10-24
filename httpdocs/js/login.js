ab88cb48c50a2746b87709207ba55dc81a959cff
megatol <74714212+megatol@users.noreply.github.com>
2023-10-24 13:29:14 +0200

    f

--- httpdocs/js/login.js
+++ httpdocs/js/login.js
@@ -1,8 +1,12 @@
+async function login(const password) {
+  const hash = await crypto.subtle.digest('SHA-256', password);
+  console.log('password = ' + password);
+  console.log('hash = ' + hash);
+}
+
 window.onload = async function() {
   document.getElementById('login').addEventListener('click', function(event) {
     const password = document.getElementById('password').value;
-    const hash = await crypto.subtle.digest('SHA-256', password);
-    console.log('password = ' + password);
-    console.log('hash = ' + hash);
+    login(password);
   });
 };
