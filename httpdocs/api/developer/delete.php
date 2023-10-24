d7e7dd53f1ca0bd281a84b748e181ba1e3f61044
megatol <74714212+megatol@users.noreply.github.com>
2023-10-24 16:49:56 +0200

    f

--- httpdocs/api/developer/delete.php
+++ httpdocs/api/developer/delete.php
@@ -32,10 +32,14 @@ while($row = $result->fetch_assoc())
   $registration_ids[] = intval($row['id']);
 $registrations = implode(',', $registration_ids);
 die("DELETE FROM registration WHERE id IN ($registrations)");
-$mysqli->query("DELETE FROM registration WHERE id IN ($registrations)") or die($mysqli->error);
-$mysqli->query("DELETE FROM publication WHERE id IN ($registrations)") or die($mysqli->error);
-$mysqli->query("DELETE FROM endorsement WHERE id IN ($endorsements)") or die($mysqli->error);
-$mysqli->query("DELETE FROM publication WHERE id IN ($endorsements)") or die($mysqli->error);
+if ($registrations !== '') {
+  $mysqli->query("DELETE FROM registration WHERE id IN ($registrations)") or die($mysqli->error);
+  $mysqli->query("DELETE FROM publication WHERE id IN ($registrations)") or die($mysqli->error);
+}
+if ($endorsements !== '') {
+  $mysqli->query("DELETE FROM endorsement WHERE id IN ($endorsements)") or die($mysqli->error);
+  $mysqli->query("DELETE FROM publication WHERE id IN ($endorsements)") or die($mysqli->error);
+}
 $mysqli->query("DELETE FROM citizen WHERE id=$id") or die($mysqli->error);
 $mysqli->query("DELETE FROM publication WHERE id=$id") or die($mysqli->error);
 die("OK");
