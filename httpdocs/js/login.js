03038ecbe4d150f8199a8b758b0f6be9005989c4
megatol <74714212+megatol@users.noreply.github.com>
2023-10-24 13:21:36 +0200

    f

--- httpdocs/js/login.js
+++ httpdocs/js/login.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   document.getElementById('login').addEventListener('click', function(event) {
     const password = document.getElementById('password').value;
-    console.log('value = ' + value);
+    console.log('value = ' + password);
   });
 };
