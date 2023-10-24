c30d6c4b891438d35469baeb02b48edab677fec4
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-10-24 14:14:33 +0200

    test

--- httpdocs/api/participation.php
+++ httpdocs/api/participation.php
@@ -28,7 +28,7 @@ if (!str_starts_with($station, 'https://'))
 $query = "SELECT publication.`version`, publication.`type`, "
         ."REPLACE(TO_BASE64(publication.`key`), '\\n', '') AS `key`, "
         ."REPLACE(TO_BASE64(publication.signature), '\\n', '') AS signature, "
-        ."UNIX_TIMESTAMP(publication.published) AS published, "
+        ."publication.published AS published, "
         ."REPLACE(TO_BASE64(participation.referendum), '\\n', '') AS referendum, "
         ."REPLACE(TO_BASE64(participation.blindKey), '\\n', '') AS blindKey FROM participation "
         ."INNER JOIN publication ON publication.id=participation.id "
