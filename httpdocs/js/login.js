d249f6d6adfdd63b3a11d7b7186fb8986790153a
megatol <74714212+megatol@users.noreply.github.com>
2023-10-24 13:31:03 +0200

    f

--- httpdocs/js/login.js
+++ httpdocs/js/login.js
@@ -1,5 +1,5 @@
 async function login(password) {
-  const hash = await crypto.subtle.digest('SHA-256', TextEncoder().encode(password));
+  const hash = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(password));
   console.log('password = ' + password);
   console.log('hash = ' + hash);
 }
