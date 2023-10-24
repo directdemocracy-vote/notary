351f1afd47a1abb2d41810ee34a8f8f91d29dd22
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-10-24 11:04:07 +0200

    typo

--- httpdocs/api/participation.php
+++ httpdocs/api/participation.php
@@ -65,11 +65,11 @@ if (!$publication) {
   }
   # $publication['schema'] looks like this: 'https://directdemocracy.vote/json-schema/2/participation.json'
   $version = intval(explode('/', $publication['schema'])[4]);
-  $publication_key = $publication['key'];
-  $publication_signature = $publication['signature']; 
-  $publication_published = $publication['published'];
+  // $publication_key = $publication['key'];
+  // $publication_signature = $publication['signature']; 
+  // $publication_published = ;
   $query = "INSERT INTO publication(`version`, `type`, `key`, `signature`, published) "
-          ."VALUES($version, 'participation', FROM_BASE64('$publication_key'), FROM_BASE64('$publication_signature'), FROM_UNIXTIME($publication_published / 1000))";
+          ."VALUES($version, 'participation', FROM_BASE64('$publication[key]'), FROM_BASE64($publication[signature]), FROM_UNIXTIME($publication[published] / 1000))";
   $mysqli->query($query) or error($mysqli->error);
   $publicationId = $mysqli->insert_id;
   $query = "INSERT INTO participation(id, referendum, blindKey, station) "
