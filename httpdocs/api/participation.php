8f6c6578a8c44267f180bd14728dc786460839d0
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-10-24 11:03:04 +0200

    typo

--- httpdocs/api/participation.php
+++ httpdocs/api/participation.php
@@ -69,7 +69,7 @@ if (!$publication) {
   $publication_signature = $publication['signature']; 
   $publication_published = $publication['published'];
   $query = "INSERT INTO publication(`version`, `type`, `key`, `signature`, published) "
-          ."VALUES($version, 'participation', FROM_BASE64('$publication_key'), FROM_BASE64('$publication_signature'), FROM_UNIXTIME(UNIX_TIMESTAMP($publication_published)))";
+          ."VALUES($version, 'participation', FROM_BASE64('$publication_key'), FROM_BASE64('$publication_signature'), FROM_UNIXTIME($publication_published / 1000))";
   $mysqli->query($query) or error($mysqli->error);
   $publicationId = $mysqli->insert_id;
   $query = "INSERT INTO participation(id, referendum, blindKey, station) "
