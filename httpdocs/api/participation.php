52f97254de0083a642e3fa05a9e3fce5f9001271
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-10-24 10:51:23 +0200

    remove die

--- httpdocs/api/participation.php
+++ httpdocs/api/participation.php
@@ -68,7 +68,6 @@ if (!$publication) {
   $publication_key = $publication['key'];
   $publication_signature = $publication['signature']; 
   $publication_published = $publication['published'];
-  die("test".$publication_published);
 
   $query = "INSERT INTO publication(`version`, `type`, `key`, `signature`, published) "
           ."VALUES($version, 'participation', FROM_BASE64('$publication_key'), FROM_BASE64('$publication_signature'), FROM_UNIXTIME(1698132613060))";
