d06db19e6dda24c5246f57d5857a2c06003974b4
megatol <74714212+megatol@users.noreply.github.com>
2023-10-26 15:13:48 +0200

    f

--- httpdocs/api/publication.php
+++ httpdocs/api/publication.php
@@ -35,7 +35,7 @@ $publication['published'] = intval($publication['published']);
 $type = $publication['type'];
 unset($publication['type']);
 if ($type == 'citizen') {
-  $query = "SELECT REPLACE(TO_BASE64(appKey), '\\n', '')) AS appKey, REPLACE(TO_BASE64(appSignature), '\\n', '')) AS appSignature, givenNames, familyName, "
+  $query = "SELECT REPLACE(TO_BASE64(appKey), '\\n', '') AS appKey, REPLACE(TO_BASE64(appSignature), '\\n', '') AS appSignature, givenNames, familyName, "
           ."CONCAT('data:image/jpeg;base64,', REPLACE(TO_BASE64(picture), '\\n', '')) AS picture, "
           ."ST_Y(home) AS latitude, ST_X(home) AS longitude FROM citizen WHERE id=$publication_id";
   $result = $mysqli->query($query) or error($mysqli->error);
@@ -46,7 +46,7 @@ if ($type == 'citizen') {
   $citizen = $publication + $citizen;
   echo json_encode($citizen, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 } elseif ($type == 'endorsement') {
-  $query = "SELECT REPLACE(TO_BASE64(appKey), '\\n', '')) AS appKey, REPLACE(TO_BASE64(appSignature), '\\n', '')) AS appSignature, `revoke`, "
+  $query = "SELECT REPLACE(TO_BASE64(appKey), '\\n', '') AS appKey, REPLACE(TO_BASE64(appSignature), '\\n', '') AS appSignature, `revoke`, "
           ."message, comment, endorsedSignature FROM endorsement WHERE id=$publication_id";
   $result = $mysqli->query($query) or error($mysqli->error);
   $endorsement = $result->fetch_assoc();
@@ -66,7 +66,7 @@ if ($type == 'citizen') {
   $proposal = $publication + $proposal;
   echo json_encode($proposal, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 } elseif ($type == 'ballot') {
-  $query = "SELECT REPLACE(TO_BASE64(appKey), '\\n', '')) AS appKey, REPLACE(TO_BASE64(appSignature), '\\n', '')) AS appSignature, "
+  $query = "SELECT REPLACE(TO_BASE64(appKey), '\\n', '') AS appKey, REPLACE(TO_BASE64(appSignature), '\\n', '') AS appSignature, "
           ."REPLACE(TO_BASE64(stationKey), '\\n', '') AS stationKey, REPLACE(TO_BASE64(stationSignature), '\\n', '') AS stationSignature, "
           ."answer from ballot WHERE id=$publication_id";
   $result = $mysqli->query($query) or error($mysqli->error);
