c2dc34c296925ead96e67f5f1e6c3b44cdad2e0c
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-11-22 16:01:24 +0100

    Update public_key.php

--- php/public_key.php
+++ php/public_key.php
@@ -2,7 +2,7 @@
 
 function public_key($key) {
   $public_key = "-----BEGIN PUBLIC KEY-----\n";
-  $key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA" + $key + "IDAQAB";
+  $key = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA' . $key . 'IDAQAB';
   $l = strlen($key);
   for($i = 0; $i < $l; $i += 64)
     $public_key .= substr($key, $i, 64) . "\n";
