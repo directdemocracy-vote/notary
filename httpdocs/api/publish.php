98f9a2f8e3d581085f90c917552829e65eeb67ba
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-08 15:09:19 +0100

    Update publish.php

--- httpdocs/api/publish.php
+++ httpdocs/api/publish.php
@@ -279,7 +279,7 @@ $mysqli->query($query) or error($mysqli->error);
 if ($type === 'proposal')
   update_corpus($mysqli, $id);
 if ($type === 'vote') {
-  $query = "INSERT INTO results VALUE(referendum, answer, `count`) VALUES(FROM_BASE64('$referendum=='), \"$answer\", 1) "
+  $query = "INSERT INTO results(referendum, answer, `count`) VALUES(FROM_BASE64('$referendum=='), \"$answer\", 1) "
           ."ON DUPLICATE KEY UPDATE `count`=`count`+1";
   $mysqli->query($query) or error($mysqli->error);
   $query = "UPDATE proposal SET participants=participants+1 LEFT JOIN publication ON publication.id=proposal.id WHERE publication.signature=FROM_BASE64('$referendum==')";
