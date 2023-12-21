892f9fd6915e9ab29685d5ab01221784ecb155df
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-21 09:55:40 +0100

    Update proposal.js

--- httpdocs/js/proposal.js
+++ httpdocs/js/proposal.js
@@ -1,31 +1,5 @@
 /* global L, QRious */
-
-const base128Charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' +
-  'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõøùúûüýÿþ@$*£¢';
-
-function encodeBase128(byteArray) { // Uint8Array
-  function toBin(byteArray) {
-    let end = '';
-    for (let i in byteArray) {
-      let aux = byteArray[i].toString(2);
-      if (aux.length < 8)
-        aux = '0'.repeat(8 - aux.length) + aux;
-      end += aux;
-    }
-    return end;
-  }
-  const bin = toBin(byteArray);
-  const sevenBits = bin.match(/.{1,7}/g);
-  while (sevenBits[sevenBits.length - 1].length < 7)
-    sevenBits[sevenBits.length - 1] += '0';
-  let res = [];
-  for (let i in sevenBits) {
-    const interger = parseInt('0' + sevenBits[i], 2);
-    res.push(base128Charset[interger]);
-  }
-  res = res.join('');
-  return res;
-}
+import {encodeBase128} from './base128.js';
 
 function findGetParameter(parameterName) {
   let result;
