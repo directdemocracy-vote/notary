fe76b27554d4698f050d5a9d02cb9f04cf8388c4
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-09 16:28:35 +0100

    Create transferred.php

---
+++ httpdocs/api/transferred.php
@@ -0,0 +1,7 @@
+<?php
+# 1. create a transfer file named after the fingerprint of the tested citizen card in ../../transfer/
+# 2. check in the database if the citizen card was reported as tranferred, if yes, return true immediately.
+# 3. if not, monitor the created file until it gets deleted, then return true.
+# 4. if the file was not deleted after 1 minute, delete it and return false.
+
+?>
