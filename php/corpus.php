92124c6dad5ad12cdc38fbeaad406cc066469356
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-15 12:11:05 +0100

    Update corpus.php

--- php/corpus.php
+++ php/corpus.php
@@ -14,7 +14,7 @@ function update_corpus($mysqli, $id) {
   INNER JOIN publication AS pp on pp.id=proposal.id
   INNER JOIN publication AS pa ON pa.`signature`=proposal.area
   INNER JOIN area ON area.id=pa.id AND ST_Contains(area.polygons, POINT(ST_X(citizen.home), ST_Y(citizen.home)))
-  INNER JOIN webservice AS judge ON judge.`type`='judge' AND judge.`key`=pp.`key` AND judge.`key`=pe.`key`
+  INNER JOIN participant AS judge ON judge.`type`='judge' AND judge.`key`=pp.`key` AND judge.`key`=pe.`key`
   WHERE certificate.`type`='endorse' OR (certificate.`type`='report' AND EXISTS($signed))
   EOT;
   $query = "UPDATE proposal SET corpus = ($count) WHERE proposal.id=$id";
