27a91673a57f3c8682fa501e761cf57a686abc70
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-15 11:12:42 +0100

    Update citizen.php

--- httpdocs/api/citizen.php
+++ httpdocs/api/citizen.php
@@ -47,10 +47,10 @@ $query = "SELECT pc.id, "
         ."CONCAT('data:image/jpeg;base64,', REPLACE(TO_BASE64(c.picture), '\\n', '')) AS picture, "
         ."ST_Y(c.home) AS latitude, ST_X(c.home) AS longitude "
         ."FROM publication pe "
-        ."INNER JOIN certificate e ON e.id = pe.id AND e.type = 'endorse' "
-        ."INNER JOIN publication pc ON pc.`key` = pe.`key` "
-        ."INNER JOIN citizen c ON pc.id = c.id "
-        ."WHERE e.publicationId = $id AND e.latest = 1 ORDER BY pe.published DESC";
+        ."INNER JOIN certificate e ON e.id=pe.id AND e.type='endorse' AND e.latest=1 "
+        ."INNER JOIN publication pc ON pc.`key`=pe.`key` "
+        ."INNER JOIN citizen c ON pc.id=c.id "
+        ."WHERE e.publicationId=$id ORDER BY pe.published DESC";
 $result = $mysqli->query($query) or die("{\"error\":\"$mysqli->error\"}");
 if (!$result)
   die("{\"error\":\"$mysqli->error\"}");
@@ -67,11 +67,10 @@ $query = "SELECT pc.id, "
         ."UNIX_TIMESTAMP(pe.published) AS published, "
         ."e.type "
         ."FROM publication pe "
-        ."INNER JOIN certificate e ON e.id = pe.id AND (e.type = 'endorse' OR (e.type = 'report' and e.comment = 'revoke')) "
+        ."INNER JOIN certificate e ON e.id=pe.id AND e.latest=1 AND (e.type='endorse' OR (e.type='report' and e.comment='revoke')) "
         ."INNER JOIN publication pc ON pc.id = e.publicationId "
         ."INNER JOIN citizen c ON pc.id = c.id "
-        ."WHERE pe.`key` = FROM_BASE64('$key==') "
-        ."AND e.latest = 1 ORDER BY pe.published DESC";
+        ."WHERE pe.id=$id ORDER BY pe.published DESC";
 $result = $mysqli->query($query) or die("{\"error\":\"$mysqli->error\"}");
 while($e = $result->fetch_assoc()) {
   settype($e['id'], 'int');
