26746fd57d9f2ad10a4c81f1311d315c38cd493d
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-17 17:54:48 +0100

    Update trusts.php

--- httpdocs/api/trusts.php
+++ httpdocs/api/trusts.php
@@ -10,11 +10,9 @@ header("Access-Control-Allow-Headers: content-type");
 if (isset($_GET['fingerprint'])) {
   $fingerprint = sanitize_field($_GET["fingerprint"], "hex", "fingerprint");
   $condition = "publication.signatureSHA1=UNHEX('$fingerprint')";
-  $join_condition = "SHA1(publication.signature)='$fingerprint'";
 } elseif (isset($_GET['signature'])) {
   $signature = sanitize_field($_GET["signature"], "base64", "signature");
   $condition = "publication.signature=FROM_BASE64('$signature==')";
-  $join_condition = "publication.signature=FROM_BASE64('$signature==')";
 } else
   error("Missing fingerprint or signature parameter");
 
@@ -36,6 +34,7 @@ $query = "SELECT UNIX_TIMESTAMP(publication.published) AS published, certificate
         ." INNER JOIN participant ON participant.`type`='judge' AND participant.id=publication.participant"
         ." INNER JOIN webservice ON webservice.participant=participant.id AND webservice.url='$judge'"
         ." INNER JOIN certificate ON certificate.certifiedPublication=publication.id AND $join_condition"
+        ." WHERE $condition"
         ." ORDER BY publication.published DESC";
 $result = $mysqli->query($query) or error($mysqli->error);
 $endorsements = array();
