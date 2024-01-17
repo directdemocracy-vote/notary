5bc5f18986d3c010b4b61495a12f9da055a9ec87
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-17 17:18:20 +0100

    Update trusts.php

--- httpdocs/api/trusts.php
+++ httpdocs/api/trusts.php
@@ -10,14 +10,13 @@ header("Access-Control-Allow-Headers: content-type");
 if (isset($_GET['fingerprint'])) {
   $fingerprint = sanitize_field($_GET["fingerprint"], "hex", "fingerprint");
   $condition = "publication.signatureSHA1=UNHEX('$fingerprint')";
-  $join_condition = "SHA1(certificate.publication)='$fingerprint'";
+  $join_condition = "SHA1(publication.signature)='$fingerprint'";
 } elseif (isset($_GET['signature'])) {
   $signature = sanitize_field($_GET["signature"], "base64", "signature");
   $condition = "publication.signature=FROM_BASE64('$signature==')";
-  $join_condition = "certificate.publication=FROM_BASE64('$signature==')";
+  $join_condition = "publication.signature=FROM_BASE64('$signature==')";
 } else
   error("Missing fingerprint or signature parameter");
-
 
 if (isset($_GET['judge']))
   $judge = sanitize_field($_GET["judge"], "url", "judge");
@@ -36,7 +35,7 @@ $answer['familyName'] = $citizen['familyName'];
 $query = "SELECT UNIX_TIMESTAMP(publication.published) AS published, certificate.type, certificate.latest FROM publication"
         ." INNER JOIN participant ON participant.`type`='judge' AND participant.id=publication.participant"
         ." INNER JOIN webservice ON webservice.participant=participant.id AND webservice.url='$judge'"
-        ." INNER JOIN certificate ON certificate.publication=publication.id AND $join_condition"
+        ." INNER JOIN certificate ON certificate.certifiedPublication=publication.id AND $join_condition"
         ." ORDER BY publication.published DESC";
 $result = $mysqli->query($query) or error($mysqli->error);
 $endorsements = array();
