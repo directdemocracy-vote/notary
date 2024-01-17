60b2b829489f3681a32f2a6405dfbc279cff91ec
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-17 13:36:06 +0100

    Update notary.sql

--- database/notary.sql
+++ database/notary.sql
@@ -1,52 +1,55 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 CREATE TABLE `area` (
-  `id` int(11) NOT NULL,
-  `name` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `publication` int(11) NOT NULL,
+  `name` varchar(1024) NOT NULL,
   `polygons` multipolygon NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `citizen` (
-  `id` int(11) NOT NULL,
-  `appId` int(11) NOT NULL,
+CREATE TABLE `certificate` (
+  `publication` int(11) NOT NULL,
+  `app` int(11) NOT NULL,
   `appSignature` blob NOT NULL,
-  `givenNames` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `familyName` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` enum('endorse','report','update','transfer','lost','sign','trust','distrust') NOT NULL,
+  `certifiedPublication` int(11) NOT NULL,
+  `comment` varchar(2048) NOT NULL,
+  `message` varchar(2048) NOT NULL,
+  `latest` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `citizen` (
+  `publication` int(11) NOT NULL,
+  `app` int(11) NOT NULL,
+  `appSignature` blob NOT NULL,
+  `givenNames` varchar(256) NOT NULL,
+  `familyName` varchar(256) NOT NULL,
   `picture` blob NOT NULL,
   `home` point NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `certificate` (
+CREATE TABLE `participant` (
   `id` int(11) NOT NULL,
-  `appId` int(11) NOT NULL,
-  `appSignature` blob NOT NULL,
-  `type` enum('endorse', 'report', 'update', 'transfer', 'lost', 'sign') NOT NULL,
-  `publicationId` int(11) NOT NULL,
-  `comment` varchar(2048) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `message` varchar(2048) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `latest` tinyint(1) NOT NULL
+  `type` enum('app','citizen','judge','notary','station') NOT NULL,
+  `key` blob NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `publication` (
-  `id` int(11) NOT NULL,
-  `version` smallint(6) NOT NULL,
-  `type` enum('citizen','certificate','area','proposal','participation','vote') NOT NULL,
-  `published` datetime NOT NULL,
-  `authorId` int(11) not NULL COMMENT 'participant id of the author',
-  `signatureSHA1` binary(20) GENERATED ALWAYS AS (unhex(sha(`signature`))) STORED,
-  `signature` blob NOT NULL COMMENT 'signature of the publication by the author'
+CREATE TABLE `participation` (
+  `publication` int(11) NOT NULL,
+  `app` int(11) NOT NULL,
+  `appSignature` blob NOT NULL,
+  `referendum` int(11) NOT NULL,
+  `encryptedVote` blob NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `proposal` (
-  `id` int(11) NOT NULL,
-  `area` blob NOT NULL,
-  `title` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `question` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `answers` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `publication` int(11) NOT NULL,
+  `area` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  `question` varchar(128) NOT NULL,
+  `answers` text NOT NULL,
   `secret` tinyint(1) NOT NULL,
   `deadline` datetime NOT NULL,
   `trust` bigint(20) NOT NULL,
@@ -56,85 +59,82 @@ CREATE TABLE `proposal` (
   `results` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `participant` (
+CREATE TABLE `publication` (
   `id` int(11) NOT NULL,
-  `type` enum('app', 'citizen', 'judge', 'notary', 'station') NOT NULL,
-  `key` blob NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-  
-CREATE TABLE `participation` (
-  `id` int(11) NOT NULL,
-  `appId` int(11) NOT NULL,
-  `appSignature` blob NOT NULL,
-  `referendum` blob NOT NULL,
-  `encryptedVote` blob NOT NULL,
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE `vote` (
-  `id` int(11) NOT NULL,
-  `appId` int(11) NOT NULL,
-  `appSignature` blob NOT NULL,
-  `referendum` blob NOT NULL,
-  `number` int(11) NOT NULL,
-  `ballot` binary(32) NOT NULL,
-  `answer` text COLLATE utf8mb4_unicode_ci NOT NULL
+  `version` smallint(6) NOT NULL,
+  `type` enum('citizen','certificate','area','proposal','participation','vote') NOT NULL,
+  `published` datetime NOT NULL,
+  `signature` blob NOT NULL COMMENT 'signature of the publication by the author',
+  `signatureSHA1` binary(20) GENERATED ALWAYS AS (unhex(sha(`signature`))) STORED,
+  `participant` int(11) NOT NULL COMMENT 'participant id of the author'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `results` (
   `referendum` int(11) NOT NULL,
-  `answer` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `answer` text NOT NULL,
   `count` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE `vote` (
+  `publication` int(11) NOT NULL,
+  `app` int(11) NOT NULL,
+  `appSignature` blob NOT NULL,
+  `referendum` int(11) NOT NULL,
+  `number` int(11) NOT NULL,
+  `ballot` binary(32) NOT NULL,
+  `answer` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 CREATE TABLE `webservice` (
-  `id` int(11) NOT NULL,
+  `participant` int(11) NOT NULL,
   `url` varchar(2048) CHARACTER SET ascii COLLATE ascii_bin NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-ALTER TABLE `area`
-  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `citizen`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `appId` (`appId`);
+ALTER TABLE `area`
+  ADD PRIMARY KEY (`publication`);
 
 ALTER TABLE `certificate`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `type` (`type`),
-  ADD KEY `publicationId` (`publicationId`),
-  ADD KEY `appId` (`appId`);
+  ADD PRIMARY KEY (`publication`),
+  ADD KEY `publicationId` (`certifiedPublication`),
+  ADD KEY `app` (`app`);
+
+ALTER TABLE `citizen`
+  ADD PRIMARY KEY (`publication`),
+  ADD KEY `app` (`app`);
+
+ALTER TABLE `participant`
+  ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `participation`
-  ADD PRIMARY KEY `id`,
-  ADD KEY `referendum` (`referendum`),
-  ADD KEY `appId` (`appId`);
+  ADD PRIMARY KEY (`publication`),
+  ADD KEY `app` (`app`),
+  ADD KEY `referendum` (`referendum`);
 
-ALTER TABLE `vote`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `referendum` (`referendum`,`ballot`) USING HASH,
-  ADD KEY `appId` (`appId`);
-
-ALTER TABLE `vote`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `proposal`
+  ADD PRIMARY KEY (`publication`),
+  ADD KEY `area` (`area`);
 
 ALTER TABLE `publication`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `signatureSHA1` (`signatureSHA1`),
-  ADD KEY `authorId` (`authorId`);
-
-ALTER TABLE `proposal`
-  ADD PRIMARY KEY (`id`);
+  ADD KEY `authorId` (`participant`);
 
 ALTER TABLE `results`
   ADD UNIQUE KEY `referendum` (`referendum`,`answer`) USING HASH;
 
-ALTER TABLE `webservice`
-  ADD KEY `id` (`id`);
+ALTER TABLE `vote`
+  ADD PRIMARY KEY (`publication`),
+  ADD KEY `app` (`app`),
+  ADD KEY `referendum` (`referendum`);
 
-ALTER TABLE `area`
+ALTER TABLE `webservice`
+  ADD PRIMARY KEY (`participant`);
+
+
+ALTER TABLE `participant`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `publication`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
 COMMIT;
