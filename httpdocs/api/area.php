fd502de970e2a24deb28dae9e9379b01e4767fbc
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-26 18:03:44 +0100

    Update area.php

--- httpdocs/api/area.php
+++ httpdocs/api/area.php
@@ -8,16 +8,21 @@ header("Access-Control-Allow-Headers: content-type");
 
 $input = json_decode(file_get_contents("php://input"));
 $judge = sanitize_field($input->judge, "base64", "judge");
-$area = $mysqli->escape_string($input->area);
-
+if (isset($input->area)) {
+  $area = $mysqli->escape_string($input->area);
+  $condition = "area.name=\"$area\" AND area.local=0";
+} else if (isset($input->lat) && isset($input->lon)) {
+  $lat = floatval($input->lat);
+  $lon = floatval($input->lon);
+  $condition = "ST_Contains(area.polygons, POINT($lon, $lat)) AND area.local=1"
+} else
+  error("Missing area and lat/lon parameters");
 if (!$judge)
   error("Missing judge argument");
-if (!$area)
-  error("Missing area argument");
 
 $query = "SELECT UNIX_TIMESTAMP(publication.published) AS published FROM area "
         ."LEFT JOIN publication ON publication.id=area.publication "
-        ."WHERE publication.`key`='$judge' AND area.name='$area' AND publication.published <= NOW()";
+        ."WHERE publication.`key`='$judge' AND $condition AND publication.published <= NOW()";
 $result = $mysqli->query($query) or error($mysqli->error);
 if (!$result)
   die("{\"status\":\"area not found\"}");
