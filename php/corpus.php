e1258a82d103b7997096e59a5d5e481f910cf582
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-30 10:12:20 +0100

    Update corpus.php

--- php/corpus.php
+++ php/corpus.php
@@ -3,7 +3,7 @@ function update_corpus($mysqli, $publication) {
   $signed = <<<EOT
   SELECT pm.id FROM publication AS pm
   INNER JOIN certificate AS m ON m.publication=pm.id AND m.type='sign'
-  WHERE pm.`key`=pc.`key` AND m.publication=pp.signature
+  WHERE pm.participant=pc.participant AND m.publication=pp.signature
   EOT;
   $count = <<<EOT
   SELECT COUNT(citizen.publication) FROM citizen
@@ -14,7 +14,7 @@ function update_corpus($mysqli, $publication) {
   INNER JOIN publication AS pp on pp.id=proposal.publication
   INNER JOIN publication AS pa ON pa.`signature`=proposal.area
   INNER JOIN area ON area.publication=pa.id AND ST_Contains(area.polygons, POINT(ST_X(citizen.home), ST_Y(citizen.home)))
-  INNER JOIN participant AS judge ON judge.`type`='judge' AND judge.`key`=pp.`key` AND judge.`key`=pe.`key`
+  INNER JOIN participant AS judge ON judge.`type`='judge' AND judge.id=pp.participant AND judge.id=pe.participant
   WHERE certificate.`type`='endorse' OR (certificate.`type`='report' AND EXISTS($signed))
   EOT;
   $query = "UPDATE proposal SET corpus = ($count) WHERE proposal.publication=$publication";
