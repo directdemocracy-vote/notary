b1b1c57343b1d50f99cacac0c511cfa827f8ad8a
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-02-01 12:02:54 +0100

    Create reputation.php

---
+++ httpdocs/api/reputation.php
@@ -0,0 +1,26 @@
+<?php
+# This a proxy to query any judge for the reputation of a citizen
+require_once '../../php/database.php';
+require_once '../../php/sanitizer.php';
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: content-type");
+
+if (!isset($_GET['key']))
+  die('{"error":"missing key argument"}');
+if (!isset($_GET['judge']))
+  die('{"error":"missing judge argument"}');
+
+$key = sanitize_field($_GET['key'], 'base64', 'key');
+$judge = sanitize_field($_GET['judge'], 'base64', 'judge');
+
+$query = "SELECT url FROM webservice INNER JOIN participant ON participant.id=webservice.participant WHERE participant.`key`=FROM_BASE64('$judge==')";
+$result = $mysqli->query($query) or error($mysqli->error);
+$j = $result->fetch_assoc();
+$result->free();
+if (!$j)
+  error('judge not found');
+$url = $j['url'];
+return file_get_content("$judge/api/reputation?key=".urlencode($key));
+?>
