b58b33f0b30a7832216cb0a9eb3c7096a20ae07e
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-02-05 14:57:28 +0100

    Update admin.php

--- httpdocs/admin/admin.php
+++ httpdocs/admin/admin.php
@@ -54,13 +54,6 @@ $n = $n_citizen + $n_certificate + $n_proposal + $n_area + $n_participation + $n
 query("DELETE FROM certificate WHERE certificate.certifiedPublication NOT IN (SELECT id FROM publication)");
 
 # clean-up orphan publications
-
-query("DELETE FROM certificate WHERE publication NOT IN (SELECT id FROM publication)");
-query("DELETE FROM participation WHERE publication NOT IN (SELECT id FROM publication)");
-query("DELETE FROM vote WHERE publication NOT IN (SELECT id FROM publication)");
-query("DELETE FROM citizen WHERE publication NOT IN (SELECT id FROM publication)");
-query("DELETE FROM area WHERE publication NOT IN (SELECT id FROM publication)");
-query("DELETE FROM proposal WHERE publication NOT IN (SELECT id FROM publication)");
 $query = <<<EOT
 DELETE FROM publication WHERE id NOT IN (
     SELECT publication FROM citizen UNION
@@ -71,6 +64,12 @@ DELETE FROM publication WHERE id NOT IN (
     SELECT publication FROM vote)
 EOT;
 query($query);
+query("DELETE FROM certificate WHERE publication NOT IN (SELECT id FROM publication)");
+query("DELETE FROM participation WHERE publication NOT IN (SELECT id FROM publication)");
+query("DELETE FROM vote WHERE publication NOT IN (SELECT id FROM publication)");
+query("DELETE FROM citizen WHERE publication NOT IN (SELECT id FROM publication)");
+query("DELETE FROM area WHERE publication NOT IN (SELECT id FROM publication)");
+query("DELETE FROM proposal WHERE publication NOT IN (SELECT id FROM publication)");
 
 $result = query("SELECT MAX(id) AS `max` FROM publication");
 if ($result) {
