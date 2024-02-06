2f9e1911a129f252fd6284f996d8f566d7b02c46
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-02-06 16:18:47 +0100

    Update notary.js

--- httpdocs/js/notary.js
+++ httpdocs/js/notary.js
@@ -321,6 +321,6 @@ function openTab(event, name) {
   document.getElementById(name).style.display = 'block';
   event.currentTarget.classList.add('is-active');
   const section = document.getElementById('proposal-results');
-  console.log('innerHTML = ' + section.innerHTML.trim() === '');
-  section.style.display = (name === 'proposals' && section.innerHTML.trim() === '') ? '' : 'none';
+  console.log('innerHTML = ' + section.childElementCount);
+  section.style.display = (name === 'proposals' && section.childElementCount !== 0) ? '' : 'none';
 }
