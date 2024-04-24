963f31f39c979ed748ff33a148e7045fb172a273
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-04-24 10:22:06 +0200

    Update publish.php

--- httpdocs/api/publish.php
+++ httpdocs/api/publish.php
@@ -161,16 +161,10 @@ if ($type === 'citizen') {
   list($app, $app_signature) = check_app($citizen);
   $family_name = $mysqli->escape_string($publication->familyName);
   $given_names = $mysqli->escape_string($publication->givenNames);
-  $latitude = isset($citizen->latitude) ? sanitize_field($citizen->latitude, 'float', 'latitude') : 0;
-  $longitude = isset($citizen->longitude) ? sanitize_field($citizen->longitude, 'float', 'longitude') : 0;
   $commune = sanitize_field($citizen->commune, 'positive_int', 'commune');
-  $query = "INSERT INTO citizen(publication, app, appSignature, familyName, givenNames, commune, picture, home) "
+  $query = "INSERT INTO citizen(publication, app, appSignature, familyName, givenNames, commune, picture) "
           ."VALUES($id, $app, FROM_BASE64('$app_signature=='), \"$family_name\", \"$given_names\", $commune, ";
-  if ($citizen_picture === '')
-    $query .= "''";
-  else
-    $query .= "FROM_BASE64('$citizen_picture')";
-  $query .= ", POINT($longitude, $latitude))";
+          ."FROM_BASE64('$citizen_picture'))";
 } elseif ($type === 'certificate') {
   $certificate = &$publication;
   if (isset($certificate->appKey))
