67f77250ee80054bbc3f8dea9e36a3257a2628eb
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-04-26 16:33:08 +0200

    Update citizens.php

--- httpdocs/api/citizens.php
+++ httpdocs/api/citizens.php
@@ -7,45 +7,37 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
 
-$radius = sanitize_field($_GET['radius'], 'positive_float', 'radius');
-if ($radius) {
-  $radius = $radius / 1000;
-  $latitude = sanitize_field($_GET["latitude"], "float", "latitude");
-  $longitude = sanitize_field($_GET["longitude"], "float", "longitude");
-}
-
+if (!isset($_GET['judge']))
+  error('Missing judge parameter');
+$commune = isset($_GET['commune']) ? sanitize_field($_GET['commune'], 'positive_int', 'commune'); : null;
+if (isset($_GET['type']))
+  $type = ($_GET['type'] === 'inactive' || $_GET['type'] === 'active') ? $_GET['type'] : '';
+else
+  $type = '';
 $familyName = isset($_GET['familyName']) ? $mysqli->escape_string($_GET['familyName']) : null;
 $givenNames = isset($_GET['givenNames']) ? $mysqli->escape_string($_GET['givenNames']) : null;
-$judge = isset($_GET['judge']) ? sanitize_field($_GET['judge'], 'url', 'judge') : null;
+$judge = sanitize_field($_GET['judge'], 'url', 'judge');
 
 $key = '';
-if ($judge) {
-  $result = $mysqli->query("SELECT REPLACE(REPLACE(TO_BASE64(`key`), '\\n', ''), '=', '') AS `key` FROM participant INNER JOIN webservice ON webservice.participant=participant.id WHERE participant.`type`='judge' AND webservice.url='$judge'") or error($mysqli->error);
-  if ($j = $result->fetch_assoc())
-    $key = $j['key'];
-  $result->free();
-}
+$result = $mysqli->query("SELECT REPLACE(REPLACE(TO_BASE64(`key`), '\\n', ''), '=', '') AS `key` FROM participant INNER JOIN webservice ON webservice.participant=participant.id WHERE participant.`type`='judge' AND webservice.url='$judge'") or error($mysqli->error);
+if ($j = $result->fetch_assoc())
+  $key = $j['key'];
+$result->free();
 
 $query = "SELECT citizen.familyName, citizen.givenNames, CONCAT('data:image/jpeg;base64,', REPLACE(TO_BASE64(citizen.picture), '\\n', '')) AS picture, "
-        ."ST_Y(citizen.home) AS latitude, ST_X(citizen.home) AS longitude";
-if ($radius)
-  $query .= ", ST_Distance_Sphere(citizen.home, POINT($longitude, $latitude), 6370.986) AS distance";
-# If ST_Distance_Sphere is not available (like in MySQL 5.6), we need to revert to this formula:
-#  $query .= ", (6370.986 * acos(cos(radians($latitude)) * cos(radians(ST_Y(citizen.home))) * cos(radians(ST_X(citizen.home)) - radians($longitude)) "
-#           ."+ sin(radians($latitude)) * sin(radians(ST_Y(citizen.home))))) AS distance";
-
-$query .= ", publication.`version`, publication.`type`, "
-         ."REPLACE(REPLACE(TO_BASE64(participant.`key`), '\\n', ''), '=', '') AS `key`, "
-         ."REPLACE(REPLACE(TO_BASE64(publication.signature), '\\n', ''), '=', '') AS signature, "
-         ."UNIX_TIMESTAMP(publication.published) AS published "
-         ."FROM citizen "
-         ."INNER JOIN publication ON publication.id = citizen.publication "
-         ."INNER JOIN participant ON participant.id=publication.participant";
-if ($key)
-  $query .= " INNER JOIN certificate ON certificate.certifiedPublication = publication.id AND certificate.type = 'trust' AND certificate.latest = 1"
-           ." INNER JOIN publication AS pe ON pe.id=certificate.publication"
-           ." INNER JOIN participant AS pep ON pep.id=pe.participant AND pep.`key` = FROM_BASE64('$key==')";
-$query .= " WHERE status='active'";
+$query.= "UNIX_TIMESTAMP(publication.published) AS published "
+        ."FROM citizen "
+        ."INNER JOIN publication ON publication.id = citizen.publication "
+        ."INNER JOIN participant ON participant.id=publication.participant ";
+if ($type === 'active')
+  $query.= "INNER JOIN certificate ON certificate.certifiedPublication = publication.id AND certificate.type = 'trust' AND certificate.latest = 1 "
+          ."INNER JOIN publication AS pe ON pe.id=certificate.publication"
+          ."INNER JOIN participant AS pep ON pep.id=pe.participant AND pep.`key` = FROM_BASE64('$key==') ";
+elseif ($type === 'inactive')
+  $query.= "LEFT JOIN certificate ON certificate.certifiedPublication = publication.id AND certificate.type = 'distrust' AND certificate.latest = 1 "
+          ."INNER JOIN publication AS pe ON pe.id=certificate.publication"
+          ."INNER JOIN participant AS pep ON pep.id=pe.participant AND pep.`key` = FROM_BASE64('$key==') ";  
+$query.= "WHERE status='active'";
 if ($familyName or $givenNames) {
   $query .= " AND";
   if ($familyName) {
@@ -56,20 +48,16 @@ if ($familyName or $givenNames) {
   if ($givenNames)
     $query .= " givenNames LIKE \"%$givenNames%\"";
 }
-if ($radius)
-  $query .= " HAVING distance < $radius ORDER BY distance";
 $query .= " LIMIT 20;";
 $result = $mysqli->query($query) or die($mysqli->error);
 $citizens = array();
 while ($citizen = $result->fetch_assoc()) {
-  $citizen['latitude'] = floatval($citizen['latitude']);
-  $citizen['longitude'] = floatval($citizen['longitude']);
   $citizen['published'] = intval($citizen['published']);
-  $citizen['schema'] = 'https://directdemocracy.vote/json-schema/' . $citizen['version'] . '/' . $citizen['type'] . 'schema.json';
   unset($citizen['type']);
   $citizens[] = $citizen;
 }
 $result->free();
+$citizens[] = $query; # debug
 echo json_encode($citizens, JSON_UNESCAPED_SLASHES);
 $mysqli->close();
 ?>
