2b45db66b901e225f9b3ee859ae7d3482ccc15ba
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-04-26 10:23:06 +0200

    Update CH-population.php

--- httpdocs/api/CH-population.php
+++ httpdocs/api/CH-population.php
@@ -1,5 +1,17 @@
 <?php
-$municipality = $_GET['municipality'];
-
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: content-type");
+$municipality = intval($_GET['municipality']);
+$data = file_get_content('CH-cc-f-01.02.03.01.csv');
+$rows = explode("\n", $data);
+$population = -1;
+foreach($rows as &$row) {
+  $details = explode(",", $row);
+  if (intval($details[0]) == $municipality) {
+    $population = intval($details[1]);
+    break;
+  }
+}
 die("{\"popupation\":$population}");
 ?>
