e1b5eec51cba476ebfab756a4100e3178dde8c3f
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-04-26 16:53:24 +0200

    Update citizens.js

--- httpdocs/js/citizens.js
+++ httpdocs/js/citizens.js
@@ -14,6 +14,28 @@ window.onload = function() {
   fetch(`api/citizens.php?commune=${commune}&judge=${judge}&type=${type}`)
     .then(response => response.json())
     .then(answer => {
+      const tbody = document.getElementById('tbody');
+      for(citizen of answer) {
+        if (!citizen.hasOwnProperty('picture'))
+          continue;
+        const tr = document.createElement('tr');
+        tbody.appendChild(tr);
+        let td = document.createElement('td');
+        tr.appendChild(td);
+        const img = document.createElement('img');
+        td.appendChild(img);
+        img.src = citizen.picture;
+        td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = citizen.familyName;
+        td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = citizen.givenNames;
+        td = document.createElement('td');
+        tr.appendChild(td);
+        const date = new Date(citizen.published * 1000);
+        td.textContent = date.toISOString();
+      }
       console.log(answer);
     });
 }
