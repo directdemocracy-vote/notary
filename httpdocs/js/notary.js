097addbfc32fc77f2e5a6e21b4163968f35f0f6d
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-04-30 11:52:13 +0200

    Update notary.js

--- httpdocs/js/notary.js
+++ httpdocs/js/notary.js
@@ -177,10 +177,6 @@ window.onload = function() {
                       population.href = ch.url;
                     } else {
                       p = populationFromP1082(wikidata.statements);
-                      console.log(nominatim);
-                      console.log(nominatim.extratags);
-                      console.log(nominatim.extratags.population);
-                      console.log(p);
                       if (p === -1 && nominatim.hasOwnProperty('extratags') && nominatim.extratags.hasOwnProperty('population')) {
                         p = nominatim.extratags.population;
                         population.href = `https://nominatim.openstreetmap.org/ui/details.html?osmtype=R&osmid=${osmId}&class=boundary`;
