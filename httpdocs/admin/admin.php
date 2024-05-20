e6432d1251e580e929ac32f681f9262d44345446
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-05-20 21:04:39 +0200

    Update admin.php

--- httpdocs/admin/admin.php
+++ httpdocs/admin/admin.php
@@ -55,7 +55,7 @@ $n_signature = $signatures ? delete_certificate($mysqli, 'sign') : 0;
 $n_participation = $participations ? delete_publication($mysqli, 'participation') : 0;
 $n_vote = $votes ? delete_publication($mysqli, 'vote') : 0;
 $n_citizen = $citizens ? delete_publication($mysqli, 'citizen') : 0;
-if ($citizen) {
+if ($citizens) {
   delete_certificate($mysqli, 'update');
   delete_certificate($mysqli, 'transfer');
 }
