32252316891cc92cd8c2ec455928b991ef56aafd
Olivier Michel <Olivier.Michel@cyberbotics.com>
2025-02-24 21:08:26 +0100

    Update trusts.php

--- httpdocs/api/trusts.php
+++ httpdocs/api/trusts.php
@@ -1,11 +1,7 @@
 <?php
+require_once '../../php/header.php';
 require_once '../../php/database.php';
 require_once '../../php/sanitizer.php';
-
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: content-type");
-
 
 if (isset($_GET['fingerprint'])) {
   $fingerprint = sanitize_field($_GET["fingerprint"], "hex", "fingerprint");
